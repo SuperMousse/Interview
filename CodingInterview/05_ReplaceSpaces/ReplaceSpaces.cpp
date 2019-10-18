fc3ff5f2ebb4a82ebc4be62e3b6bba27b4d95ae0
SuperMousse <lcq286227341@qq.com>
2019-10-18 20:20:32 +0800

    Update ReplaceSpaces.cpp

--- CodingInterview/05_ReplaceSpaces/ReplaceSpaces.cpp
+++ CodingInterview/05_ReplaceSpaces/ReplaceSpaces.cpp
@@ -21,3 +21,54 @@ void replace_blank(string& str)
 		beg += replace_str.length();
 	}
 }
+
+void replace_blank(char *str, int length) {
+	if (str == nullptr || length <= 0)
+		return;
+	int before = 0;
+	int after = 0;
+	int blank_count = 0;
+	int real_length = 0;
+	int i = 0;
+	while (str[i] != '\0') {
+		if (str[i] == ' ') {
+			++blank_count;
+		}
+		++real_length;
+		++i;
+	}
+	int new_length = real_length + blank_count * 2;
+	if (new_length > length) {
+		return;
+	}
+	before = real_length;
+	after = real_length + blank_count * 2;
+
+	while (before != after && before >=0 && after >= 0) {
+		cout << before << after << endl;
+		if (str[before] == ' ') {
+			str[after--] = '0';
+			str[after--] = '2';
+			str[after--] = '%';
+		}
+		else {
+			str[after--] = str[before];
+		}
+		--before;
+	}
+
+}
+
+
+int main(int argc, char* argv[]){
+	//vector<int> num = {2, 3, 5, 4, 3, 2, 6, 7};
+	const int length = 100;
+	char words[length] = "We are the champion!";
+	char* str = words;
+
+	replace_blank(str, length);
+
+	cout << str << endl;
+
+	return 0;
+}
