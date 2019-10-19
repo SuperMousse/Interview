bc6ff70e9f5d16c3b655b2edf98ba83d733aa372
SuperMousse <lcq286227341@qq.com>
2019-10-19 16:10:46 +0800

    Update PrintListInReversedOrder.cpp

--- CodingInterview/06_PrintListInReversedOrder/PrintListInReversedOrder.cpp
+++ CodingInterview/06_PrintListInReversedOrder/PrintListInReversedOrder.cpp
@@ -1,4 +1,13 @@
-int main(int argc, char* argv[]){
-
-	return 0;
+vector<int> printListFromTailToHead(ListNode* head) {
+	deque<int> temp;
+	if (head != nullptr) {
+		ListNode* pNode = head;
+		while (pNode->next != nullptr) {
+			temp.push_front(pNode->val);
+			pNode = pNode->next;
+		}
+		temp.push_front(pNode->val);
+	}
+	vector<int> result(temp.begin(), temp.end());
+	return result;
 }
