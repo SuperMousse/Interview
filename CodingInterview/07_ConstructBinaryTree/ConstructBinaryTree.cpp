697f66400fe0507108e661650f48f58f9a7dff0d
SuperMousse <lcq286227341@qq.com>
2019-10-20 11:37:23 +0800

    Update ConstructBinaryTree.cpp

--- CodingInterview/07_ConstructBinaryTree/ConstructBinaryTree.cpp
+++ CodingInterview/07_ConstructBinaryTree/ConstructBinaryTree.cpp
@@ -15,38 +15,36 @@ TreeNode* reConstructBinaryTree(vector<int> pre, vector<int> vin) {
 	return ConstructCore(pre.begin(), pre.end()-1, vin.begin(), vin.end()-1);
 }
 
-TreeNode *ConstructCore(it startPreorder, it endPreorder, it startInorder, it endInorder) {
-	// The first number in Preorder sequence is root number.
+TreeNode* ConstructCore(it startPreorder, it endPreorder, it startInorder, it endInorder) {
 	int rootValue = *startPreorder;
-	TreeNode* root = new TreeNode(rootValue);
-	root->left = root->right = nullptr;
-	if (startPreorder == endPreorder) {
-		if (startPreorder == endPreorder && *startPreorder == *endPreorder) {
-			return root;
-		}
-		else {
-			throw exception("Invalid Input");
-		}
-	}
-	//find root number in Inorder sequence.
-	it rootInorder = startInorder;
-	while (rootInorder <= endInorder && *rootInorder != rootValue) {
-		++rootInorder;
-	}
-	if (rootInorder == endInorder && *rootInorder != rootValue) {
-		throw exception("Invalid Input");
-	}
+    	TreeNode* root = new TreeNode(rootValue);
+    	root->left = root->right = nullptr;
+    	// The first number in Preorder sequence is root number
+    	if (startPreorder == endPreorder) {
+    		if (startPreorder == endPreorder && *startPreorder == *endPreorder) {
+    			return root;
+    		}
+    		else {
+    			throw runtime_error("Invalid Input");
+    		}
+    	}
 
-	int leftLength = rootInorder - startInorder;
-	it leftPreorderEnd = startPreorder + leftLength;
-	if (leftLength > 0) {
-		// construct left subtree
-		root->left = ConstructCore(startPreorder+1, leftPreorderEnd, startInorder, rootInorder-1);
-	}
-	if(leftLength<endPreorder-startPreorder){
-		// construct right subtree
-		root->right = ConstructCore(leftPreorderEnd + 1, endPreorder, rootInorder+1, endInorder);
-	}
+	    // find root number in Inorder sequence
+    	it rootInorder = startInorder;
+    	while (rootInorder <= endInorder && *rootInorder != rootValue)
+	    	++rootInorder;
+    	if (rootInorder == endInorder && *rootInorder != rootValue)
+	    	throw runtime_error("Invalid Input");
 
-	return root;
+	    int leftLength = rootInorder - startInorder;
+	    it leftPreorderEnd = startPreorder + leftLength;
+	    //construct left subtree
+	    if (leftLength > 0) {
+	    	root->left = ConstructCore(startPreorder+1, leftPreorderEnd, startInorder, rootInorder-1);
+	    }
+	    // construct right subtree
+	    if (leftLength < (endPreorder - startPreorder)) {
+	    	root->right = ConstructCore(leftPreorderEnd+1, endPreorder, rootInorder+1, endInorder);
+	    }
+	    return root;
 }
