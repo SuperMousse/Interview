fb4b1b5290e7ec0a1483265b54a06dc87383667e
SuperMousse <lcq286227341@qq.com>
2019-10-20 16:14:56 +0800

    Update QueueWithTwoStacks.cpp

--- CodingInterview/09_QueueWithTwoStacks/QueueWithTwoStacks.cpp
+++ CodingInterview/09_QueueWithTwoStacks/QueueWithTwoStacks.cpp
@@ -27,3 +27,50 @@ private:
 	stack<int> stack1;
 	stack<int> stack2;
 };
+
+
+// realize stack with two queue
+class Solution
+{
+public:
+	void push(int node) {
+		if (queue1.empty() && queue2.empty()) {
+			queue1.push(node);
+		}
+		else if (!queue1.empty()) {
+			queue1.push(node);
+		}
+		else if(!queue2.empty()) {
+			queue2.push(node);
+		}
+	}
+
+	int pop() {
+		int head = 0;
+		if (!queue1.empty() && queue2.empty()) {
+			for (int i = 0; i < queue1.size()-1; ++i)
+			{
+				int data = queue1.front();
+				queue1.pop();
+				queue2.push(data);
+			}
+			int head = queue1.front();
+			queue1.pop();
+		}
+		if (queue1.empty() && !queue2.empty()) {
+			for (int i = 0; i < queue2.size() - 1; ++i)
+			{
+				int data = queue2.front();
+				queue2.pop();
+				queue1.push(data);
+			}
+			int head = queue2.front();
+			queue2.pop();
+		}
+		return head;
+	}
+
+private:
+	queue<int> queue1;
+	queue<int> queue2;
+};
