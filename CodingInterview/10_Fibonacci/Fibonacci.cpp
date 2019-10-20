d2536087b3e4218a3103aa6a39daab753429e94a
SuperMousse <lcq286227341@qq.com>
2019-10-20 16:55:37 +0800

    Create Fibonacci.cpp

---
+++ CodingInterview/10_Fibonacci/Fibonacci.cpp
@@ -0,0 +1,22 @@
+
+class Solution {
+public:
+	int Fibonacci(int n) {
+		if (n == 0 || n == 1) {
+			return n;
+		}
+		else {
+			int pre = 0;
+			int post = 1;
+			int result = 0;
+			for (int i = 0; i < n-1; ++i) {
+				result = pre + post;
+				// swap
+				int temp = post;
+				pre = temp;
+				post = result;
+			}
+			return result;
+		}
+	}
+};
