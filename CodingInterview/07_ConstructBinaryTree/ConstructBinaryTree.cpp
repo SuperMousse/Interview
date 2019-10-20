07a70240c3b196ba5c3ad8dac28942191c7797ef
SuperMousse <lcq286227341@qq.com>
2019-10-20 13:24:40 +0800

    Update ConstructBinaryTree.cpp

--- CodingInterview/07_ConstructBinaryTree/ConstructBinaryTree.cpp
+++ CodingInterview/07_ConstructBinaryTree/ConstructBinaryTree.cpp
@@ -48,3 +48,44 @@ TreeNode* ConstructCore(it startPreorder, it endPreorder, it startInorder, it en
 	    }
 	    return root;
 }
+
+
+
+struct TreeNode* reConstructBinaryTree(vector<int> pre, vector<int> in) {
+	 if (pre.empty() || in.empty() || (pre.size()!=in.size()))
+		 return nullptr;
+	 int length = pre.size();
+	 if (length == 0)
+		 return nullptr;
+
+	 vector<int> left_pre, right_pre, left_in, right_in;
+	 //创建根节点，根节点肯定是前序遍历的第一个数
+	 TreeNode * head = new TreeNode(pre[0]);
+	 //找到中序遍历根节点所在位置,存放于变量root中
+	 int root = 0;
+	 for (int i = 0; i < length; i++)
+	 {
+		 if (in[i] == pre[0])
+		 {
+			 root = i;
+			 break;
+		 }
+	 }
+	 //对于中序遍历，根节点左边的节点位于二叉树的左边，根节点右边的节点位于二叉树的右边
+	 //利用上述这点，对二叉树节点进行归并
+	 for (int i = 0; i < root; i++)
+	 {
+		 left_pre.push_back(pre[i + 1]);//前序第一个为根节点
+		 left_in.push_back(in[i]);
+	 } 
+	 for (int i = root + 1; i < length; i++)
+	 {
+		 right_pre.push_back(pre[i]);
+		 right_in.push_back(in[i]);
+	 }
+	 //和shell排序的思想类似，取出前序和中序遍历根节点左边和右边的子树
+	 //递归，再对其进行上述所有步骤，即再区分子树的左、右子子数，直到叶节点
+	 head->left = reConstructBinaryTree(left_pre, left_in);
+	 head->right = reConstructBinaryTree(right_pre, right_in);
+	 return head;
+ }
