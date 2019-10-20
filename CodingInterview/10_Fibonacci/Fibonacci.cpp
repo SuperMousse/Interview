0a4e0d94ff2bd05acb99b8da89e257e0891ec5bf
SuperMousse <lcq286227341@qq.com>
2019-10-20 21:11:26 +0800

    Update Fibonacci.cpp

--- CodingInterview/10_Fibonacci/Fibonacci.cpp
+++ CodingInterview/10_Fibonacci/Fibonacci.cpp
@@ -19,3 +19,26 @@ public:
 		}
 	}
 };
+
+
+// 快速幂递归
+long long binpow(long long a, long long b) {
+	if (b == 0) return 1;
+	long long res = binpow(a, b / 2);
+	if (b % 2)
+		return res * res * a;
+	else
+		return res * res;
+}
+
+// 快速幂非递归
+long long binpow(long long a, long long b) {
+	long long res = 1;
+	while (b > 0) {
+		if (b & 1) 
+			res = res * a;
+		a = a * a;
+		b >>= 1;
+	}
+	return res;
+}
