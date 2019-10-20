7b22210756484e13151278d6a789a7da1e862211
SuperMousse <lcq286227341@qq.com>
2019-10-20 15:30:44 +0800

    Update QueueWithTwoStacks.cpp

--- CodingInterview/09_QueueWithTwoStacks/QueueWithTwoStacks.cpp
+++ CodingInterview/09_QueueWithTwoStacks/QueueWithTwoStacks.cpp
@@ -1,1 +1,29 @@
-main
+// realize queue with two stack
+class Solution
+{
+public:
+	void push(int node) {
+		stack1.push(node);
+	}
+
+	int pop() {
+		if (stack2.empty()) {
+			while (!stack1.empty()) {
+				int data = stack1.top();
+				stack1.pop();
+				stack2.push(data);
+			}
+		}
+		if (stack2.empty()) {
+			throw runtime_error("queue is empty");
+		}
+
+		int head = stack2.top();
+		stack2.pop();
+		return head;
+	}
+
+private:
+	stack<int> stack1;
+	stack<int> stack2;
+};
