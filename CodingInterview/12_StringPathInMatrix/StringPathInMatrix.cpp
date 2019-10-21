931cc20cf9675226afe03bfde7deb1447c2e1acb
SuperMousse <lcq286227341@qq.com>
2019-10-21 21:53:30 +0800

    Create StringPathInMatrix.cpp

---
+++ CodingInterview/12_StringPathInMatrix/StringPathInMatrix.cpp
@@ -0,0 +1,51 @@
+bool hasPath(char* matrix, int rows, int cols, char* str)
+{
+	if (matrix == nullptr || rows < 1 || cols < 1 || str == nullptr) {
+		return false;
+	}
+	// visited matrix init
+	bool* visited = new bool[rows * cols];
+	for (int i = 0; i < rows * cols; ++i) {
+		visited[i] = 0;
+	}
+
+	int pathLength = 0;
+	for (int row = 0; row < rows; ++row) {
+		for (int col = 0; col < cols; ++col) {
+			if (hasPathCore(matrix, rows, cols, row, col, str, pathLength, visited)) {
+				return true;
+			}
+		}
+	}
+
+	delete[] visited;
+	return false;
+}
+
+
+
+bool hasPathCore(const char* matrix, int rows, int cols, int row, int col, 
+		            const char* str, int& pathLength, bool* visited) {
+	if (str[pathLength] == '\0') {
+		return true;
+	}
+
+	bool hasPath = false;
+	if (row >= 0 && row < rows && col >= 0 && col < cols
+		&& matrix[row * cols + col] == str[pathLength]
+		&& !visited[row * cols + col]) {
+		++pathLength;
+		visited[row * cols + col] = true;
+
+		hasPath = hasPathCore(matrix, rows, cols, row - 1, col, str, pathLength, visited)
+				    || hasPathCore(matrix, rows, cols, row + 1, col, str, pathLength, visited)
+				    || hasPathCore(matrix, rows, cols, row, col - 1, str, pathLength, visited)
+					|| hasPathCore(matrix, rows, cols, row, col + 1, str, pathLength, visited);
+
+		if (!hasPath) {
+			--pathLength;
+			visited[row * cols + col] = false;
+		}
+	}
+	return hasPath;
+}
