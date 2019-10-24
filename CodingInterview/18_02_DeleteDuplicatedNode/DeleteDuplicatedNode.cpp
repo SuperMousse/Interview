d4f3a38ab6202e99f8ddbc4b8c9dd78ff3bc8d78
SuperMousse <lcq286227341@qq.com>
2019-10-24 19:31:30 +0800

    Create DeleteDuplicatedNode.cpp

---
+++ CodingInterview/18_02_DeleteDuplicatedNode/DeleteDuplicatedNode.cpp
@@ -0,0 +1,39 @@
+ListNode* deleteDuplication(ListNode* pHead)
+{
+	if (pHead == nullptr) {
+		return pHead;
+	}
+	ListNode* pPre = nullptr;
+	ListNode* pCurrent = pHead;
+
+	while (pCurrent != nullptr) {
+		ListNode* pNext = pCurrent->next;
+		bool needDelete = false;
+		if (pNext != nullptr && pNext->val == pCurrent->val) {
+			needDelete = true;
+		}
+		if (!needDelete) {
+			pPre = pCurrent;
+			pCurrent = pNext;
+		}
+		else {
+			int value = pCurrent->val;
+			ListNode* pToBeDel = pCurrent;
+			while (pToBeDel != nullptr && pToBeDel->val == value) {
+				pNext = pToBeDel->next;
+
+				delete pToBeDel;
+				pToBeDel = nullptr;
+				pToBeDel = pNext;
+			}
+			if (pPre == nullptr) {
+				pHead = pNext;
+			}
+			else {
+				pPre->next = pNext;
+			}
+			pCurrent = pNext;
+		}
+	}
+	return pHead;
+}
