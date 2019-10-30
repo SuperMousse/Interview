ee07f18809ea87551bb1d5e38327c63740b08bf1
SuperMousse <lcq286227341@qq.com>
2019-10-30 20:03:12 +0800

    Update Print1ToMaxOfNDigits.cpp

--- CodingInterview/17_Print1ToMaxOfNDigits/Print1ToMaxOfNDigits.cpp
+++ CodingInterview/17_Print1ToMaxOfNDigits/Print1ToMaxOfNDigits.cpp
@@ -64,3 +64,50 @@ void PrintNumber(char* number) {
 
 
 // 解法二
+void Print1ToMaxOfNDigitsRecursively(char* number, int length, int index);
+void PrintNumber(char* number);
+
+void Print1ToMaxOfNDigits(int n) {
+	if (n <= 0) {
+		return;
+	}
+	char* number = new char[n + 1];
+	memset(number, '0', n);
+	number[n] = '\0';
+
+	for (int i = 0; i < 10; ++i) {
+		number[0] = i + '0';
+		Print1ToMaxOfNDigitsRecursively(number, n, 0);
+	}
+
+	delete[] number;
+	return;
+}
+
+void Print1ToMaxOfNDigitsRecursively(char* number, int length, int index) {
+	if (index == length - 1) {
+		PrintNumber(number);
+		return;
+	}
+	for (int i = 0; i < 10; ++i) {
+		number[index + 1] = i + '0';
+		Print1ToMaxOfNDigitsRecursively(number, length, index + 1);
+	}
+}
+
+void PrintNumber(char* number) {
+	bool isBeginning0 = true;
+	int nLength = strlen(number);
+
+	for (int i = 0; i < nLength; ++i) {
+		if (isBeginning0 && number[i] != '0') {
+			isBeginning0 = false;
+		}
+		if (!isBeginning0) {
+			cout << number[i] << " ";
+		}
+	}
+
+	cout << "\t";
+}
+
