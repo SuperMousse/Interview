58ca6aa5916f73bc0fd6a3fda776025746763908
SuperMousse <lcq286227341@qq.com>
2019-11-10 16:04:13 +0800

    Update ReorderArray.cpp

--- CodingInterview/21_ReorderArray/ReorderArray.cpp
+++ CodingInterview/21_ReorderArray/ReorderArray.cpp
@@ -19,3 +19,12 @@ void reOrderArray(vector<int>& array) {
 }
 
 // 牛客网上还要求交换后奇数与奇数，偶数与偶数的相对位置不变
+void reOrderArray(vector<int>& array) {
+	stable_partition(array.begin(), array.end(), isOk); // stable partition根据谓词把序列分成两个子列，并且保持原有的序列顺序
+}
+
+
+bool isOk(int n) {
+	return ((n & 1) == 1);　//奇数返回真
+}
+
