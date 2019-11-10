16e071765c338a3887756a3428abdceee5cf4882
SuperMousse <lcq286227341@qq.com>
2019-11-10 16:34:20 +0800

    Update ReorderArray.cpp

--- CodingInterview/21_ReorderArray/ReorderArray.cpp
+++ CodingInterview/21_ReorderArray/ReorderArray.cpp
@@ -18,6 +18,31 @@ void reOrderArray(vector<int>& array) {
 	}
 }
 
+
+// 剑指offer进阶解法，使用函数指针，可以将这个问题泛化，每次传进isEven或者其他函数的指针即可
+bool isEven(int n) {
+	return (n & 1) == 0;
+}
+
+void reOrderArray(vector<int>& array, bool (*func)(int)) {
+	if (array.empty())
+		return;
+	unsigned int start = 0;
+	unsigned int end = array.size() - 1;
+	while (start < end) {
+		while (start < end && !func(array[start]))
+			++start;
+		while (start < end && func(array[start]))
+			--end;
+		if (start < end) {
+			int temp = array[start];
+			array[start] = array[end];
+			array[end] = temp;
+		}
+
+	}
+}
+
 // 牛客网上还要求交换后奇数与奇数，偶数与偶数的相对位置不变
 void reOrderArray(vector<int>& array) {
 	stable_partition(array.begin(), array.end(), isOk); // stable partition根据谓词把序列分成两个子列，并且保持原有的序列顺序
