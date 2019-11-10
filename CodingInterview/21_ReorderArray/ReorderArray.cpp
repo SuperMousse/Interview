df0c03b75fd740c63770acba4bb891f182377c25
SuperMousse <lcq286227341@qq.com>
2019-11-10 17:11:41 +0800

    Update ReorderArray.cpp

--- CodingInterview/21_ReorderArray/ReorderArray.cpp
+++ CodingInterview/21_ReorderArray/ReorderArray.cpp
@@ -57,3 +57,39 @@ bool isOk(int n) {
 void reOrderArray(vector<int>& array) {
 	stable_partition(array.begin(), array.end(), [](int n) {return ((n & 1) == 1); }); // stable partition根据谓词把序列分成两个子列，并且保持原有的序列顺序
 }
+
+
+/**
+ * 1.要想保证原有次序，则只能顺次移动或相邻交换。
+ * 2.i从左向右遍历，找到第一个偶数。
+ * 3.j从i+1开始向后找，直到找到第一个奇数。
+ * 4.将[i,...,j-1]的元素整体后移一位，最后将找到的奇数放入i位置，然后i++。
+ * 5.終止條件：j向後遍歷查找失敗。
+ */
+void reOrderArray(vector<int>& array) {
+	if (array.empty())
+		return;
+	unsigned int before = 0;
+	unsigned int after = before + 1;
+	while (before < array.size()) {
+		while (before < array.size() && (array[before] % 2 != 0)) {
+			++before;
+		}
+		after = before + 1;
+		while (after < array.size() && (array[after] % 2 == 0)) {
+			++after;
+		}
+		if (after < array.size()) {
+			int temp = array[after];
+			for (unsigned int i = before + 1; i <= after; ++i) {
+				array[i] = array[i - 1];
+			}
+			array[before] = temp;
+		}
+		else {
+			break;
+		}
+		
+	}
+
+}
