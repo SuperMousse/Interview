a3300b7ffde58acf584fee29477093c0fd3b463b
SuperMousse <lcq286227341@qq.com>
2019-11-10 22:50:37 +0800

    Update ReverseList.cpp

--- CodingInterview/24_ReverseList/ReverseList.cpp
+++ CodingInterview/24_ReverseList/ReverseList.cpp
@@ -30,3 +30,18 @@ ListNode* ReverseList(ListNode* pHead) {
 
 
 // 递归解法
+ListNode* ReverseList(ListNode* pHead) {
+	//如果链表为空或者链表中只有一个元素
+	if (pHead == nullptr || pHead->next == nullptr) 
+		return pHead;
+
+	//先反转后面的链表，走到链表的末端结点
+	ListNode * pReverseNode = ReverseList(pHead->next);
+
+	//再将当前节点设置为后面节点的后续节点
+	pHead->next->next = pHead;
+	pHead->next = nullptr;
+
+	return pReverseNode;
+
+}
