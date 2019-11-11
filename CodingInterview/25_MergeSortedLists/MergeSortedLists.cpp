d5947b1afe913547634ff791763cba70ccd5f12a
SuperMousse <lcq286227341@qq.com>
2019-11-11 10:08:40 +0800

    Update MergeSortedLists.cpp

--- CodingInterview/25_MergeSortedLists/MergeSortedLists.cpp
+++ CodingInterview/25_MergeSortedLists/MergeSortedLists.cpp
@@ -26,3 +26,52 @@ ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
 	}
 	return pMergedHead;
 }
+
+
+// 非递归解法
+ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
+{
+	if (pHead1 == nullptr) {
+		return pHead2;
+	}
+	else if (pHead2 == nullptr) {
+		return pHead1;
+	}
+
+	ListNode* pMergedHead = nullptr;
+	ListNode* pCurrent = nullptr;
+	while (pHead1 != nullptr && pHead2 !=nullptr)
+	{
+		if (pHead1->val<pHead2->val)
+		{
+			if (pMergedHead == nullptr) {
+				pMergedHead = pHead1;
+				pCurrent = pHead1;
+			}
+			else {
+				pCurrent->next = pHead1;
+				pCurrent = pCurrent->next;
+			}
+			pHead1 = pHead1->next;
+		}
+		else {
+			if (pMergedHead == nullptr) {
+				pMergedHead = pHead2;
+				pCurrent = pHead2;
+			}
+			else {
+				pCurrent->next = pHead2;
+				pCurrent = pCurrent->next;
+			}
+			pHead2 = pHead2->next;
+		}
+	}
+
+	if (pHead1 == nullptr) {
+		pCurrent->next = pHead2;
+	}
+	else {
+		pCurrent->next = pHead1;
+	}
+	return pMergedHead;
+}
