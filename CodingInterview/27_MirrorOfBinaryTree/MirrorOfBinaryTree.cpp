6db9825cec48d1127124d91e1c5673e5a08832a9
SuperMousse <lcq286227341@qq.com>
2019-11-13 22:07:36 +0800

    Create MirrorOfBinaryTree.cpp

---
+++ CodingInterview/27_MirrorOfBinaryTree/MirrorOfBinaryTree.cpp
@@ -0,0 +1,33 @@
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+
+
+void Mirror(TreeNode* pRoot) {
+	// pRoot is null
+	if (pRoot == nullptr) {
+		return;
+	}
+	// only sigle node 
+	if (pRoot->left == nullptr && pRoot->right == nullptr) {
+		return;
+	}
+
+	TreeNode* pTemp = pRoot->left;
+	pRoot->left = pRoot->right;
+	pRoot->right = pTemp;
+
+	// mirror if the node is not a leaf node
+	if (pRoot->left != nullptr) {
+		Mirror(pRoot->left);
+	}
+	if (pRoot->right != nullptr) {
+		Mirror(pRoot->right);
+	}
+	return;
+}
