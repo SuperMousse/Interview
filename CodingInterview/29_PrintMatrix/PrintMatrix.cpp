4cdb33876b0ea6e7812643de15a784837df40758
SuperMousse <lcq286227341@qq.com>
2019-11-14 17:26:53 +0800

    Update PrintMatrix.cpp

--- CodingInterview/29_PrintMatrix/PrintMatrix.cpp
+++ CodingInterview/29_PrintMatrix/PrintMatrix.cpp
@@ -58,3 +58,58 @@ vector<int> printMatrixInCircle(vector<vector<int> > matrix, unsigned int rows,
 }
 
 
+
+// 牛客网上的解法，使用4个label标记
+vector<int> printMatrix(vector<vector<int> > matrix) {
+	int rows = (int)matrix.size();
+	int columns = (int)matrix[0].size();
+	vector<int> result;
+	if (matrix.empty() || rows == 0 || columns == 0) {
+		return result;
+	}
+	int up = 0;
+	int down = rows - 1;
+	int left = 0;
+	int right = columns - 1;
+	while (true) {
+		// 最上面一行
+		for (int col = left; left <= right; ++col) {
+			result.push_back(matrix[up][col]);
+		}
+		++up; // 向下
+		// 判断是否越界
+		if (up > down) {
+			break;
+		}
+		// 最右边一列
+		for (int row = up; row <= down; ++row) {
+			result.push_back(matrix[row][right]);
+		}
+		--right; //向左
+		// 判断是否越界
+		if (left > right) {
+			break;
+		}
+		// 最下面一行
+		for (int col = up; col >= left; --col) {
+			result.push_back(matrix[down][col]);
+		}
+		--down; // 向上
+		// 判断是否越界
+		if (up > down) {
+			break;
+		}
+		// 最左边一行
+		for (int row = down; row >= up; row--) {
+			result.push_back(matrix[row][left]);
+		}
+		// 向右
+		++left;
+		// 判断是否越界
+		if (left > right) {
+			break;
+		}
+
+	}
+	return result;
+}
