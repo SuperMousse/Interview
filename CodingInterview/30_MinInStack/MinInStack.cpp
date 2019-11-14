7f28374b1bb00705fc884da6f55aff792984a30c
SuperMousse <lcq286227341@qq.com>
2019-11-14 21:17:03 +0800

    Update MinInStack.cpp

--- CodingInterview/30_MinInStack/MinInStack.cpp
+++ CodingInterview/30_MinInStack/MinInStack.cpp
@@ -24,3 +24,30 @@ private:
 	stack<int> data_stack;
 	stack<int> min_stack;
 };
+
+
+
+
+class Solution {
+public:
+	void push(int value) {
+		data_stack.push(value);
+		if (min_stack.empty() || value < min_stack.top())
+			min_stack.push(value);
+
+	}
+	void pop() {
+		if (min_stack.top() == data_stack.top())
+			min_stack.pop();
+		data_stack.pop();
+	}
+	int top() {
+		return data_stack.top();
+	}
+	int min() {
+		return min_stack.top();
+	}
+private:
+	stack<int> data_stack;
+	stack<int> min_stack;
+};
