5d5fa3e9e260cd9b58ec3f9139c34e12d70c972f
SuperMousse <lcq286227341@qq.com>
2019-11-14 21:16:47 +0800

    Create MinInStack.cpp

---
+++ CodingInterview/30_MinInStack/MinInStack.cpp
@@ -0,0 +1,26 @@
+// 
+class Solution {
+public:
+	void push(int value) {
+		data_stack.push(value);
+		if (min_stack.empty() || value < min_stack.top()) {
+			min_stack.push(value);
+		}
+		else {
+			min_stack.push(data_stack.top());
+		}
+	}
+	void pop() {
+		min_stack.pop();
+		data_stack.pop();
+	}
+	int top() {
+		return data_stack.top();
+	}
+	int min_() {
+		return min_stack.top();
+	}
+private:
+	stack<int> data_stack;
+	stack<int> min_stack;
+};
