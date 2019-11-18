9de41b333a6cd3c73bf14d4a2f753274a2b225a6
SuperMousse <lcq286227341@qq.com>
2019-11-18 10:48:27 +0800

    Create ConvertBinarySearchTree.cpp

---
+++ CodingInterview/36_ConvertBinarySearchTree/ConvertBinarySearchTree.cpp
@@ -0,0 +1,14 @@
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+}
+
+TreeNode* Convert(TreeNode* pRootOfTree)
+{
+
+
+}
