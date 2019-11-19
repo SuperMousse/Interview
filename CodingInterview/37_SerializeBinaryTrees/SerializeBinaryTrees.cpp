019d2d6fa1d7e9ac8de0cc51cec90333b274868f
SuperMousse <lcq286227341@qq.com>
2019-11-19 14:26:13 +0800

    Update SerializeBinaryTrees.cpp

--- CodingInterview/37_SerializeBinaryTrees/SerializeBinaryTrees.cpp
+++ CodingInterview/37_SerializeBinaryTrees/SerializeBinaryTrees.cpp
@@ -7,13 +7,14 @@ struct TreeNode {
 	}
 };
 
+
 char* Serialize(TreeNode* root) {
 	char* result = nullptr;
 	if (root == nullptr) {
 		return result;
 	}
 	string str;
-	Serialize(root, str);
+	SerializeCore(root, str);
 	result = new char[str.length() + 1];
 	int i = 0;
 	for (i = 0; i < str.length(); i++) {
@@ -23,7 +24,7 @@ char* Serialize(TreeNode* root) {
 	return result;
 }
 
-void Serialize(TreeNode* root, string& str) {
+void SerializeCore(TreeNode* root, string& str) {
 	if (root == nullptr) {
 		str += '#';
 		return;
@@ -31,6 +32,40 @@ void Serialize(TreeNode* root, string& str) {
 	string r = to_string(root->val);
 	str += r;
 	str += ',';
-	Serialize(root->left, str);
-	Serialize(root->right, str);
+	SerializeCore(root->left, str);
+	SerializeCore(root->right, str);
 }
+
+TreeNode* Deserialize(char* str) {
+	TreeNode* root = nullptr;
+	if (str == nullptr) {
+		return root;
+	}
+	root = DeserializeCore(&str);
+	return root;
+}
+
+
+TreeNode* DeserializeCore(char** str) { // 递归时字符串指针还需要向后移动, 若只是使用指针则无法影响函数外部
+	if (**str == '#') {
+		++(*str);
+		return nullptr;
+	}
+	int num = 0;
+	while (**str != '\0' && **str != ',') {
+		num = num * 10 + ((**str) - '0');
+		++(*str);
+	}
+
+	TreeNode* root = new TreeNode(num);
+	if (**str == '\0') {
+		return root;
+	}
+	else {
+		++(*str);
+	}
+	root->left = DeserializeCore(str);
+	root->right = DeserializeCore(str);
+	return root;
+}
+
