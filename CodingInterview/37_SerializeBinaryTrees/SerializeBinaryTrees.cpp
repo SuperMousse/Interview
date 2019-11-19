25194464539fcc725ea9e4ab3586654647650f30
SuperMousse <lcq286227341@qq.com>
2019-11-19 11:03:53 +0800

    Create SerializeBinaryTrees.cpp

---
+++ CodingInterview/37_SerializeBinaryTrees/SerializeBinaryTrees.cpp
@@ -0,0 +1,16 @@
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+
+
+char* Serialize(TreeNode* root) {
+
+}
+TreeNode* Deserialize(char* str) {
+
+}
