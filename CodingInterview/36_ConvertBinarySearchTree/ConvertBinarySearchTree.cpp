ddb4617ebc85ead7376c56b9c15213a2b3971ede
SuperMousse <lcq286227341@qq.com>
2019-11-19 10:05:04 +0800

    Update ConvertBinarySearchTree.cpp

--- CodingInterview/36_ConvertBinarySearchTree/ConvertBinarySearchTree.cpp
+++ CodingInterview/36_ConvertBinarySearchTree/ConvertBinarySearchTree.cpp
@@ -1,14 +1,37 @@
-struct TreeNode {
-	int val;
-	struct TreeNode *left;
-	struct TreeNode *right;
-	TreeNode(int x) :
-			val(x), left(NULL), right(NULL) {
-	}
-}
-
+// 剑指offer解法
 TreeNode* Convert(TreeNode* pRootOfTree)
 {
+	TreeNode* pLastNodeInList = nullptr;
+	ConvertNode(pRootOfTree, &pLastNodeInList);
 
+	// pLastNodeInList指向双向链表的尾节点
+	// 我们需要返回头结点
+	TreeNode* pHeadOfList = pLastNodeInList;
+	while (pHeadOfList != nullptr && pHeadOfList->left != nullptr) {
+		pHeadOfList = pHeadOfList->left;
+	}
 
+	return pHeadOfList;
+}
+
+void ConvertNode(TreeNode* pNode, TreeNode** pLastNodeInList) {
+	if (pNode == nullptr) {
+		return;
+	}
+	TreeNode* pCurrent = pNode;
+
+	if (pCurrent->left != nullptr) {
+		ConvertNode(pCurrent->left, pLastNodeInList);
+	}
+
+	pCurrent->left = *pLastNodeInList;
+	if (*pLastNodeInList != nullptr) {
+		(*pLastNodeInList)->right = pCurrent;
+	}
+
+	*pLastNodeInList = pCurrent;
+
+	if (pCurrent->right != nullptr) {
+		ConvertNode(pCurrent->right, pLastNodeInList);
+	}
 }
