f851dfddc016c16899ea2a2ee9ee23b0927fef59
SuperMousse <lcq286227341@qq.com>
2019-11-20 20:13:24 +0800

    Update MoreThanHalfNumber.cpp

--- CodingInterview/39_MoreThanHalfNumber/MoreThanHalfNumber.cpp
+++ CodingInterview/39_MoreThanHalfNumber/MoreThanHalfNumber.cpp
@@ -25,3 +25,75 @@ int MoreThanHalfNum_Solution(vector<int> numbers) {
 
 
 // 剑指offer解法
+// 解法一, 利用quick sort的partition函数来查找中位数
+int MoreThanHalfNum_Solution(vector<int> numbers) {
+	if (numbers.empty()) {
+		return 0;
+	}
+	int length = numbers.size();
+	int middle = length >> 1;
+	int start = 0;
+	int end = length - 1;
+	int index = Partition(numbers, start, end);
+	while (index != middle) {
+		if (index > middle) {
+			end = index - 1;
+			index = Partition(numbers, start, end);
+		}
+		else {
+			start = index + 1;
+			index = Partition(numbers, start, end);
+		}
+	}
+
+	int result = numbers[middle];
+	// 确认result次数是否真的超过了n/2
+	if (!CheckMoreThanHalf(numbers, result)) {
+		return 0;
+	}
+
+	return result;
+}
+
+bool CheckMoreThanHalf(vector<int>& numbers, int result) {
+	if (numbers.empty()) {
+		return false;
+	}
+	int times = 0;
+	for (int i = 0; i < numbers.size(); ++i){
+		if (numbers[i] == result) {
+			++times;
+		}
+	}
+
+	bool isMoreThanHalf = true;
+	if (times * 2 <= numbers.size()) {
+		return false;
+	}
+	return isMoreThanHalf;
+}
+
+
+int Partition(vector<int>& numbers, int start, int end) {
+	if (numbers.empty()) {
+		return 0;
+	}
+	int base = start;
+	int left = start;
+	int right = end;
+	while (true) {
+		while (left<right && numbers[right]>numbers[base]) {
+			--right;
+		}
+		while (left < right && numbers[left] <= numbers[base]) {
+			++left;
+		}
+		if (left == right) {
+			break;
+		}
+		int temp = numbers[left];
+		numbers[left] = numbers[right];
+		numbers[right] = temp;
+	}
+	return left;
+}
