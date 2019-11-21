a3c2aacb47a26fd0301bfd17f242835901a42fbd
SuperMousse <lcq286227341@qq.com>
2019-11-21 21:08:44 +0800

    Create GreatestSumOfSubarrays.cpp

---
+++ CodingInterview/42_GreatestSumOfSubarrays/GreatestSumOfSubarrays.cpp
@@ -0,0 +1,26 @@
+// 剑指offer解法
+// 将最大的子数组和初始化为最小的负数
+
+int FindGreatestSumOfSubArray(vector<int> array) {
+	if (array.empty()) {
+		return 0;
+	}
+	int nCurSum = 0;
+	int nGreatestSum = 0x80000000; // 负数最小值
+	for (int i = 0; i < array.size(); ++i) {
+		if (nCurSum <= 0) {
+			nCurSum = array[i];
+		}
+		else {
+			nCurSum += array[i];
+		}
+		if (nCurSum > nGreatestSum) {
+			nGreatestSum = nCurSum;
+		}
+	}
+	return nGreatestSum;
+
+}
+
+// 牛客网解法
+// 将最大的子数组和初始化为array[0]
