5107277e32e4a57f5db4ea44f239fe91f8d982a4
SuperMousse <lcq286227341@qq.com>
2019-11-21 22:14:36 +0800

    Update NumberOfK.cpp

--- CodingInterview/53_01_NumberOfK/NumberOfK.cpp
+++ CodingInterview/53_01_NumberOfK/NumberOfK.cpp
@@ -1,4 +1,4 @@
-// 直观解法, 时间复杂度O(N)
+// 直观解法一, 顺序扫描, 时间复杂度O(N)
 int GetNumberOfK(vector<int> data, int k) {
 	if (data.empty()) {
 		return 0;
@@ -10,3 +10,9 @@ int GetNumberOfK(vector<int> data, int k) {
 	}
 	return numbers;
 }
+
+
+// 解法二, 二分查找+左右扫描, 由于顺序扫描时间复杂度为时间复杂度O(N), 则该算法最差时间复杂度为O(N)
+
+
+// 解法三, 二分查找first value+二分查找last value, 时间复杂度O(logN)
