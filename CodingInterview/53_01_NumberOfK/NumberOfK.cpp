e97c949522e3baf35ee3ffd8386cb8955d396255
SuperMousse <lcq286227341@qq.com>
2019-11-21 21:49:02 +0800

    Update NumberOfK.cpp

--- CodingInterview/53_01_NumberOfK/NumberOfK.cpp
+++ CodingInterview/53_01_NumberOfK/NumberOfK.cpp
@@ -1,1 +1,12 @@
-// 
+// 直观解法, 时间复杂度O(N)
+int GetNumberOfK(vector<int> data, int k) {
+	if (data.empty()) {
+		return 0;
+	}
+	int numbers = 0;
+	for (int i = 0; i < data.size(); ++i) {
+		if (data[i] == k)
+			++numbers;
+	}
+	return numbers;
+}
