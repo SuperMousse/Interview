8a82859fef01993e01de9cc011baee6f730a245d
SuperMousse <lcq286227341@qq.com>
2019-11-21 21:29:34 +0800

    Update NumberOf1.cpp

--- CodingInterview/43_NumberOf1/NumberOf1.cpp
+++ CodingInterview/43_NumberOf1/NumberOf1.cpp
@@ -1,1 +1,27 @@
-xxx
+// 最简单解法
+// 若输入数字n, 有O(logN)位, 若判断每个数字的每一位是否含1, 总复杂度为O(NlogN)
+int NumberOf1Between1AndN_Solution(int n)
+{
+	if (n < 1) {
+		return 0;
+	}
+	int number = 0;
+	for (int i = 1; i <= n; ++i) {
+		number += NumberOf1(i);
+	}
+	return number;
+}
+
+int NumberOf1(int n) {
+	int number = 0;
+	while (n) {
+		if (n % 10 == 1) {
+			++number;
+		}
+		n = n / 10;
+	}
+	return number;
+}
+
+
+// 
