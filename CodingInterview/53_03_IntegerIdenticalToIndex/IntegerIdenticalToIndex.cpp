05519ca6239bce1150a00f13eb353c360b1c7269
SuperMousse <lcq286227341@qq.com>
2019-11-22 10:53:13 +0800

    Update IntegerIdenticalToIndex.cpp

--- CodingInterview/53_03_IntegerIdenticalToIndex/IntegerIdenticalToIndex.cpp
+++ CodingInterview/53_03_IntegerIdenticalToIndex/IntegerIdenticalToIndex.cpp
@@ -1,1 +1,23 @@
-xxx
+
+int GetNumberSameAsIndex(vector<int> numbers) {
+	if (numbers.empty()) {
+		return -1;
+	}
+	int length = numbers.size();
+	int start = 0;
+	int end = numbers.size() - 1;
+	int middle = (start + end) >> 2;
+	while (start <= end) {
+		if (numbers[middle] == middle) {
+			return middle;
+		}
+		else if (numbers[middle] > middle) {
+			end = middle - 1;
+		}
+		else {
+			start = middle + 1;
+		}
+	}
+
+	return -1;
+}
