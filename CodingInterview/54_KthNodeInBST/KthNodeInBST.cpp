d0aff9c02b2bba8633fd9b28fd05f1af6293587e
SuperMousse <lcq286227341@qq.com>
2019-11-22 13:52:08 +0800

    Update KthNodeInBST.cpp

--- CodingInterview/54_KthNodeInBST/KthNodeInBST.cpp
+++ CodingInterview/54_KthNodeInBST/KthNodeInBST.cpp
@@ -1,1 +1,32 @@
-xxx
+// 递归中序遍历
+
+TreeNode* KthNode(TreeNode* pRoot, int k)
+{
+	if (pRoot == nullptr || k <= 0) {
+		return nullptr;
+	}
+	vector<TreeNode*> result;
+	KthNodeCore(pRoot, k, result);
+
+	if (k > result.size()) {
+		return nullptr;
+	}
+	return result[k - 1];
+}
+
+void KthNodeCore(TreeNode* pRoot, int k, vector<TreeNode*>& result) {
+	if (pRoot == nullptr) {
+		return;
+	}
+	if (pRoot->left != nullptr) {
+		KthNodeCore(pRoot->left, k, result);
+	}
+	result.push_back(pRoot);
+	if (pRoot->right != nullptr) {
+		KthNodeCore(pRoot->right, k, result);
+	}
+
+	return;
+}
+
+// 非递归中序遍历
