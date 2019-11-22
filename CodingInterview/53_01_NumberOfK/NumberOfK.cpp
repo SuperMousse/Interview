5534b55f09e031c75919c0a9eb3b695451cf2df5
SuperMousse <lcq286227341@qq.com>
2019-11-22 09:26:36 +0800

    Update NumberOfK.cpp

--- CodingInterview/53_01_NumberOfK/NumberOfK.cpp
+++ CodingInterview/53_01_NumberOfK/NumberOfK.cpp
@@ -16,3 +16,61 @@ int GetNumberOfK(vector<int> data, int k) {
 
 
 // 解法三, 二分查找first value+二分查找last value, 时间复杂度O(logN)
+
+int GetNumberOfK(vector<int> data, int k) {
+	if (data.empty()) {
+		return 0;
+	}
+	int numbers = 0;
+	int first = GetFirstK(data, k, 0, data.size() - 1);
+	int last = GetLastK(data, k, 0, data.size() - 1);
+
+	if (first >= 0 && last >= 0) {
+		numbers = last - first + 1;
+	}
+	return numbers;
+}
+
+int GetFirstK(vector<int>&data, int k, int start, int end) {
+	if (start > end) {
+		return -1;
+	}
+	int middle = (start + end) / 2;
+	if (data[middle] == k) {
+		if ((middle > 0 && data[middle - 1] != k) || middle == 0) {
+			return middle;
+		}
+		else {
+			end = middle - 1;
+		}
+	}
+	else if (data[middle] < k) {
+		start = middle + 1;
+	}
+	else {
+		end = middle - 1;
+	}
+	return GetFirstK(data, k, start, end);
+}
+
+int GetLastK(vector<int>& data, int k, int start, int end) {
+	if (start > end) {
+		return -1;
+	}
+	int middle = (start + end) / 2;
+	if (data[middle] == k) {
+		if ((middle < data.size() - 1 && data[middle + 1] != k) || middle == data.size() - 1) {
+			return middle;
+		}
+		else {
+			start = middle + 1;
+		}
+	}
+	else if (data[middle] < k) {
+		start = middle + 1;
+	}
+	else {
+		end = middle - 1;
+	}
+	return GetLastK(data, k, start, end);
+}
