f9e78345d47b364316052723d257ae32dce0aede
SuperMousse <lcq286227341@qq.com>
2019-11-22 10:27:50 +0800

    Update MissingNumber.cpp

--- CodingInterview/53_02_MissingNumber/MissingNumber.cpp
+++ CodingInterview/53_02_MissingNumber/MissingNumber.cpp
@@ -1,1 +1,22 @@
-xxx
+// 傻瓜解法一
+// 1~n求和 - 数组元素求和
+int GetMissingNumber(vector<int> data, int k) {
+	if (data.empty()) {
+		return -1;
+	}
+	int length = data.size();
+	int arraySum = 0;
+	int totalSum = 0;
+	for (int i = 0; i < length; ++i)
+	{
+		arraySum += data[i];
+	}
+	for (int i = 0; i < data[length - 1]; ++i) {
+		totalSum += i;
+	}
+	return totalSum - arraySum;
+
+}
+
+
+// 二分法, 利用下标和元素是否相等
