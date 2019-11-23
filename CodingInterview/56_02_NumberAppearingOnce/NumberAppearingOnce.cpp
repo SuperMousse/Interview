f1f7a4233a71810a52edee7766e8b118106eb309
SuperMousse <lcq286227341@qq.com>
2019-11-23 14:26:51 +0800

    Update NumberAppearingOnce.cpp

--- CodingInterview/56_02_NumberAppearingOnce/NumberAppearingOnce.cpp
+++ CodingInterview/56_02_NumberAppearingOnce/NumberAppearingOnce.cpp
@@ -1,1 +1,22 @@
-xxx
+int FindNumsAppearOnce(vector<int> data) {
+	if (data.empty() || data.size() < 4) {
+		throw runtime_error("wrong input");
+	}
+	int bitSum[32] = { 0 };
+	for (int i = 0; i < data.size(); ++i) {
+		int bitMask = 1;
+		for (int j = 0; j < data.size(); ++j) {
+			int bit = data[i] & bitMask;
+			if (bit != 0) {
+				++bitSum[31 - j]; // bitSum[31]存储最低位的二进制的和
+			}
+			bitMask = bitMask << 1;
+		}
+	}
+	int result = 0;
+	for (int i = 0; i < 32; ++i) {
+		result = result << 1;
+		result += bitSum[i] % 3;
+	}
+	return result;
+}
