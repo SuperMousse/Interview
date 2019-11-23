fe66e1d21265bd1b475d897789f1d52a6d83d341
SuperMousse <lcq286227341@qq.com>
2019-11-23 21:00:04 +0800

    Update ReverseWordsInSentence.cpp

--- CodingInterview/58_01_ReverseWordsInSentence/ReverseWordsInSentence.cpp
+++ CodingInterview/58_01_ReverseWordsInSentence/ReverseWordsInSentence.cpp
@@ -42,3 +42,46 @@ void Reverse(string& str, int begin, int end) {
 }
 
 // 牛客网解法
+    string ReverseSentence(string str) {
+        if (str.empty()) {
+            return str;
+        }
+
+        int mark = 0;
+        int length = str.size();
+        str += ' ';
+        for (int i = 0; i < length + 1; ++i)
+        {
+            if (str[i] == ' ') {
+                Reverse(str, mark, i - 1);
+                mark = i + 1;
+            }
+        }
+
+        str = str.substr(0, length);
+
+        // 翻转整个句子
+        Reverse(str, 0, length - 1);
+
+        return str;
+    }
+
+
+// 我自己的解法
+// follow牛客网的解法， 使用了STL的reverse接口
+// 关键在于添加句尾的' ', 才能让句子最后面的单词完成翻转
+// Hello, world
+// 如果不添加句尾的‘ ’， world无法正常翻转
+string ReverseSentence(string str) {
+	reverse(str.begin(), str.end());
+	str = str + ' ';
+	string::iterator mark = str.begin();
+	for (string::iterator it = str.begin(); it != str.end(); ++it) {
+		if (*it == ' ') {
+			reverse(mark, it);
+			mark = it + 1;
+		}
+	}
+	str = str.substr(0, str.size() - 1);
+	return str;
+}
