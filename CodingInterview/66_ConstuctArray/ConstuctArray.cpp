e6be16d6ec16d5af67afe98b4339c7c366e5b55a
SuperMousse <lcq286227341@qq.com>
2019-11-24 22:38:30 +0800

    Update ConstuctArray.cpp

--- CodingInterview/66_ConstuctArray/ConstuctArray.cpp
+++ CodingInterview/66_ConstuctArray/ConstuctArray.cpp
@@ -22,3 +22,24 @@ vector<int> multiply(const vector<int>& A) {
 
 
 // 我自己的解法
+vector<int> multiply(const vector<int>& A) {
+	vector<int> result;
+	if (A.empty()) {
+		return result;
+	}
+
+	int length = A.size();
+	vector<int> C(length, 0);
+	vector<int> D(length, 0);
+	C[0] = 1;
+	D[length - 1] = 1;
+	for (int i = 1; i < length; ++i) {
+		C[i] = C[i - 1] * A[i - 1];
+		D[length - 1 - i] = D[length - i] * A[length - i];
+	}
+
+	for (int i = 0; i < length; ++i) {
+		result.push_back((C[i] * D[i]));
+	}
+	return result;
+}
