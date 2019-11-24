208e7d8a1665cf24b2bd70e1ab6b4d1d75e7577c
SuperMousse <lcq286227341@qq.com>
2019-11-24 10:24:15 +0800

    Create QueueWithMax.cpp

---
+++ CodingInterview/59_02_QueueWithMax/QueueWithMax.cpp
@@ -0,0 +1,45 @@
+template <typename T> class QueueWithMax {
+public:
+	QueueWithMax() :currentIndex(0) {
+
+	}
+
+	void push_back(T number) {
+		while (!maximums.empty() && number >= maximums.back().number) {
+			maximums.pop_back();
+		}
+		InternalData internalData = { number, currentIndex };
+		data.push_back(internalData);
+		maximums.push_back(internalData);
+
+		++currentIndex;
+	}
+
+	void pop_front() {
+		if (maximums.empty()) {
+			throw new exception("queue is empty");
+		}
+		
+		if (maximums.front().index == data.front().index) {
+			maximums.pop_front();
+		}
+
+		data.pop_front();
+	}
+
+	T _max() const {
+		if (maximums.empty()) {
+			throw new exception("queue is empty");
+		}
+		return maximums.front().number;
+	}
+
+private:
+	struct InternalData {
+		T number;
+		int index;
+	};
+	deque<InternalData> data;
+	deque<InternalData> maximums;
+	int currentIndex;
+};
