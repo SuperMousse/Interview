723457dbcdfd3abfc2329bde3edf17dcd8293044
SuperMousse <lcq286227341@qq.com>
2019-11-25 14:31:18 +0800

    Update AddTwoNumbers.cpp

--- CodingInterview/65_AddTwoNumbers/AddTwoNumbers.cpp
+++ CodingInterview/65_AddTwoNumbers/AddTwoNumbers.cpp
@@ -26,3 +26,17 @@ int Add(int num1, int num2)
 
 
 // 位运算减法
+
+// [x-y]补=[x]补-[y]补=[x]补+[-y]补
+// 从[y]补求[-y]补的法则是：对[y]补包括符号位“求反且最末位加1”,即可得到[-y]补
+int negative(int n)
+{
+	return add(~n, 1);
+}
+
+int sub(int a, int b)
+{
+	return add(a, negative(b));
+}
+
+
