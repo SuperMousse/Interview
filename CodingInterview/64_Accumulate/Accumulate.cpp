7fe480708e6adc76eb068fd2f37fde81a485bc93
SuperMousse <lcq286227341@qq.com>
2019-11-25 16:01:20 +0800

    Update Accumulate.cpp

--- CodingInterview/64_Accumulate/Accumulate.cpp
+++ CodingInterview/64_Accumulate/Accumulate.cpp
@@ -53,3 +53,47 @@ public:
 
 
 // 剑指offer解法二
+
+
+
+
+// 剑指offer解法三: 函数指针
+typedef int (*func)(int);
+
+// 递归的终止函数
+int Teminator(int n){
+	return 0;
+}
+
+//  递归函数, 选择函数进行递归
+int Recursion(int n){
+	static func pf[2] ={Teminator, Recursion};
+
+	return n + pf[!!n](n - 1);
+}
+
+
+class Solution
+{
+public:
+	int Sum_Solution(int n){
+		return Recursion(n);
+	}
+
+};
+
+
+// 剑指offer解法四: 模版类型
+template <unsigned int n>
+struct Sum{
+	enum Value{
+		N = Sum< n - 1 >::N + n
+	};
+};
+
+template <>
+struct Sum<1>{
+	enum Value{
+		N = 1
+	};
+};
