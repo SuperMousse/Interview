4e5c9b402a7fa5694f4e551187f95ba12b18feaa
SuperMousse <lcq286227341@qq.com>
2019-11-25 15:47:22 +0800

    Update Accumulate.cpp

--- CodingInterview/64_Accumulate/Accumulate.cpp
+++ CodingInterview/64_Accumulate/Accumulate.cpp
@@ -7,3 +7,49 @@ int Sum_Solution(int n) {
 
 
 // 剑指offer解法一
+class Temp
+{
+public:
+	Temp()
+	{
+		++N;
+		Sum += N;
+	}
+
+	static void Reset()
+	{
+		N = 0;
+		Sum = 0;
+	}
+
+	static unsigned int GetSum()
+	{
+		return Sum;
+	}
+
+private:
+	static unsigned int N;
+	static unsigned int Sum;
+};
+
+unsigned int Temp::N = 0;
+unsigned int Temp::Sum = 0;
+
+class Solution
+{
+public:
+	int res;
+	int Sum_Solution(int n)
+	{
+		Temp::Reset();
+
+		Temp* a = new Temp[n];
+		delete[] a;
+		a = NULL;
+
+		return Temp::GetSum();
+	}
+};
+
+
+// 剑指offer解法二
