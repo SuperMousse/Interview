7f22a221ca5c30dacc2b3feecba7488cc2a7d0fc
SuperMousse <lcq286227341@qq.com>
2019-11-25 16:01:57 +0800

    Update Accumulate.cpp

--- CodingInterview/64_Accumulate/Accumulate.cpp
+++ CodingInterview/64_Accumulate/Accumulate.cpp
@@ -6,7 +6,7 @@ int Sum_Solution(int n) {
 }
 
 
-// 剑指offer解法一
+// 剑指offer解法一: 构造函数求解
 class Temp
 {
 public:
@@ -52,7 +52,45 @@ public:
 };
 
 
-// 剑指offer解法二
+// 剑指offer解法二: 虚函数求解
+class base;
+base *parray[2];
+
+class base{
+public:
+    virtual unsigned int sum(unsigned int n)
+    {
+        //  递归的终止
+        return 0;
+    }
+};
+
+class derive :public base{
+public:
+    virtual unsigned int sum(unsigned int n)
+    {
+        //  !!n两次非运算, 从而将n转换为bool类型, 来选择函数
+        //  sum(n) = sum(n - 1) + n 则是递归公式
+        return parray[!!n]->sum(n - 1) + n;
+    }
+};
+
+
+class Solution
+{
+public:
+    int Sum_Solution(int n)
+    {
+
+        base a;
+        derive b;
+
+        parray[0] = &a;
+        parray[1] = &b;
+
+        return parray[1]->sum(n);
+    }
+};
 
 
 
