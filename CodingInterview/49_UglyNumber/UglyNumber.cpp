07605b250c3d3fed1632679f5ccb19f26845b764
SuperMousse <lcq286227341@qq.com>
2019-11-26 10:00:47 +0800

    Update UglyNumber.cpp

--- CodingInterview/49_UglyNumber/UglyNumber.cpp
+++ CodingInterview/49_UglyNumber/UglyNumber.cpp
@@ -1,14 +1,16 @@
 // 解法一: 直接解法, 遍历每一数字
+// 该解法用时几十秒, 牛客网无法正常AC
 int GetUglyNumber_Solution(int index) {
 	if (index < 1) {
 		return 0;
 	}
 	int number = 0;
 	int uglyFound = 0;
-	while (uglyFound <= index) {
+	while (uglyFound < index) {
 		++number;
 		if (IsUgly(number)) {
 			++uglyFound;
+			cout << uglyFound << ":" << number << endl;
 		}
 	}
 
