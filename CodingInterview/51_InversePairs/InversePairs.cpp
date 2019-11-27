e1588dcfeda33454a86028578bb993b9d78d4fe5
SuperMousse <lcq286227341@qq.com>
2019-11-27 13:24:33 +0800

    Update InversePairs.cpp

--- CodingInterview/51_InversePairs/InversePairs.cpp
+++ CodingInterview/51_InversePairs/InversePairs.cpp
@@ -7,6 +7,7 @@
 4.(a / b) % c = ( ( a % c ) / ( b % c ) ) % c(不成立)
 */
 
+
 int InversePairs(vector<int> data) {
 	if (data.empty()) {
 		return 0;
@@ -16,8 +17,9 @@ int InversePairs(vector<int> data) {
 	vector<int> copy(data);
 
 	int count = InversePairsCore(data, copy, 0, length - 1); // 逆序对数目在data长度很长时有可能超出最大int的限制
+	                                                         // 1000000007是最小的十位质数,模1000000007,可以保证值永远在int的范围内
 
-	return count;
+	return count % 1000000007;
 }
 
 long long InversePairsCore(vector<int>& data, vector<int>& copy, int start, int end) {
@@ -28,8 +30,8 @@ long long InversePairsCore(vector<int>& data, vector<int>& copy, int start, int 
 
 	int length = (end - start) / 2;
 
-	int left = InversePairsCore(copy, data, start, start + length);  
-	int right = InversePairsCore(copy, data, start + length + 1, end);
+	long long left = InversePairsCore(copy, data, start, start + length);  // 二分O(logN)
+	long long right = InversePairsCore(copy, data, start + length + 1, end);
 
 	// i初始化为前半段最后一个数字的下标
 	int i = start + length;
@@ -53,56 +55,5 @@ long long InversePairsCore(vector<int>& data, vector<int>& copy, int start, int 
 		copy[indexCopy--] = data[j];
 	}
 
-	return (left + right + count)%1000000007; // 1000000007是最小的十位质数,模1000000007,可以保证值永远在int的范围内
+	return (left + right + count); 
 }
-class Solution {
-public:
-	ListNode* FindFirstCommonNode(ListNode* pHead1, ListNode* pHead2) {
-		if (pHead1 == nullptr || pHead2 == nullptr) {
-			return nullptr;
-		}
-		unsigned int nLength1 = GetListLength(pHead1);
-		unsigned int nLength2 = GetListLength(pHead2);
-
-		// init
-		int nLengthDiff = 0;
-		ListNode* pListHeadLong = nullptr;
-		ListNode* pListHeadShort = nullptr;
-		if (nLength1 < nLength2) {
-			pListHeadShort = pHead1;
-			pListHeadLong = pHead2;
-			nLengthDiff = nLength2 - nLength1;
-		}
-		else {
-			pListHeadShort = pHead2;
-			pListHeadLong = pHead1;
-			nLengthDiff = nLength1 - nLength2;
-		}
-
-		// 先在长链表上走几步, 再同时在两个链表上遍历
-		for (int i = 0; i < nLengthDiff; ++i){
-			pListHeadLong = pListHeadLong->next;
-		}
-
-		while (pListHeadLong != nullptr && pListHeadShort != nullptr && (pListHeadLong != pListHeadShort)) {
-			pListHeadLong = pListHeadLong->next;
-			pListHeadShort = pListHeadShort->next;
-		}
-
-		// 得到第一个公共节点
-		ListNode* pFirstCommonNode = pListHeadLong;
-
-		return pFirstCommonNode;
-
-	}
-
-	unsigned int GetListLength(ListNode* pHead) {
-		unsigned int nLength = 0;
-		ListNode* pNode = pHead;
-		while (pNode != nullptr) {
-			++nLength;
-			pNode = pNode->next;
-		}
-		return nLength;
-	}
-};
