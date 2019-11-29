f8541cb5443291ea5a3466abbb812e73177cc26b
SuperMousse <lcq286227341@qq.com>
2019-11-29 14:16:03 +0800

    Update CommonParentInTree.cpp

--- CodingInterview/68_CommonParentInTree/CommonParentInTree.cpp
+++ CodingInterview/68_CommonParentInTree/CommonParentInTree.cpp
@@ -51,3 +51,60 @@ TreeNode* GetLastCommonParent(TreeNode* pRoot, TreeNode* pNode1, TreeNode* pNode
 // 四、
 // 普通的树, 寻找两节点的最低公共节点, 树不含有指向父节点的指针, 使用辅助内存
 	
+struct TreeNode
+{
+	int                  val;
+	vector<TreeNode*>    children;
+	TreeNode(int x) :val(x) { };
+};
+
+TreeNode* GetLastCommonParent(TreeNode* pRoot, TreeNode* pNode1, TreeNode* pNode2) {
+	TreeNode* pRes = nullptr;
+	if (pRoot == nullptr || pNode1 == nullptr || pNode2 == nullptr) {
+		return pRes;
+	}
+
+	list<TreeNode*> path1;
+	GetNodePath(pRoot, pNode1, path1);
+
+	list<TreeNode*> path2;
+	GetNodePath(pRoot, pNode2, path2);
+
+	return GetLastCommonNode(path1, path2);
+}
+
+bool GetNodePath(TreeNode* pRoot, TreeNode* pNode, list<TreeNode*>& path) {
+	if (pRoot == pNode) {
+		return true;
+	}
+	path.push_back(pRoot);
+
+	bool found = false;
+	vector<TreeNode*>::iterator it = pRoot->children.begin();
+	while (!found && it != pRoot->children.end()) {
+		found = GetNodePath(*it, pNode, path);
+		++it;
+	}
+	if (!found) {
+		path.pop_back();
+	}
+	return found;
+}
+
+TreeNode* GetLastCommonNode(const list<TreeNode*>& path1, const list<TreeNode*>& path2) {
+	TreeNode* pLast = nullptr;
+	if (path1.empty() || path2.empty()) {
+		return pLast;
+	}
+	list<TreeNode*>::const_iterator it1 = path1.begin();
+	list<TreeNode*>::const_iterator it2 = path2.begin();
+
+	while (it1 != path1.end() && it2 != path2.end()) {
+		if (*it1 == *it2) {
+			pLast = *it1;
+		}
+		++it1;
+		++it2;
+	}
+	return pLast;
+}
