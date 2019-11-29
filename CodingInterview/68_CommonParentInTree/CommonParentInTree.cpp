c46853b081c90a949389bd84d850711076012d6f
SuperMousse <lcq286227341@qq.com>
2019-11-29 13:30:57 +0800

    Update CommonParentInTree.cpp

--- CodingInterview/68_CommonParentInTree/CommonParentInTree.cpp
+++ CodingInterview/68_CommonParentInTree/CommonParentInTree.cpp
@@ -1,8 +1,18 @@
+// 一、
 // 寻找二叉搜索树两个节点的最低公共祖先
 // 事实上是在寻找第一个位于两个节点之间的一个节点
 // 当前节点大于寻找的两个节点时, 最低公共祖先在左子树上,
 // 反之, 在右子树上
 // 若第一个介于两个搜索节点之间的节点已经找到, 该节点为两节点的最低公共祖先
+
+
+struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+
+	TreeNode(int x) : val(x), left(NULL), right(NULL) { };
+};
 
 TreeNode* GetLastCommonParent(TreeNode* pRoot, TreeNode* pNode1, TreeNode* pNode2) {
 	TreeNode* pRes = nullptr;
@@ -22,4 +32,10 @@ TreeNode* GetLastCommonParent(TreeNode* pRoot, TreeNode* pNode1, TreeNode* pNode
 		(pRoot->val >= pNode2->val && pRoot->val <= pNode1->val)) {
 		return pRoot;
 	}
+	
+}
+
+
+// 二、
+// 普通的树, 寻找两节点的最低公共祖先
 	
