85ad770b24fb4330e94f7f159c643ff27dd1a277
SuperMousse <lcq286227341@qq.com>
2019-11-30 14:03:38 +0800

    Update ValidParentheses.cpp

--- LeetCode/20_ValidParentheses/ValidParentheses.cpp
+++ LeetCode/20_ValidParentheses/ValidParentheses.cpp
@@ -1,1 +1,31 @@
-xxx
+class Solution {
+public:
+	bool isValid(string s) {
+		if (s.empty()) {
+			return true;
+		}
+		stack<char> parenthesis;
+		unordered_map<char, char> mapping = { {')', '('}, {']', '['}, {'}', '{'} };
+		int length = s.size();
+
+		for (int i = 0; i < length; ++i)
+		{
+			char c = s[i];
+			// closing bracket
+			if (mapping.count(c) != 0) {
+				char topElement = parenthesis.empty() ? '#' : parenthesis.top();
+				if (topElement == mapping[c]) {
+					parenthesis.pop(); // top element match, pop
+				}
+				else {
+					return false;
+				}
+			}
+			else {
+				parenthesis.push(c); // opening bracket
+			}
+		}
+
+		return parenthesis.empty();
+	}
+};
