b9c5e6de25ddc2be190cd15385d5e3987a07bea4
SuperMousse <lcq286227341@qq.com>
2019-11-30 16:15:52 +0800

    Update MaximumSubarray.cpp

--- LeetCode/53_MaximumSubarray/MaximumSubarray.cpp
+++ LeetCode/53_MaximumSubarray/MaximumSubarray.cpp
@@ -1,1 +1,24 @@
-xxx
+// 动态规划O(N)解法
+int maxSubArray(vector<int>& nums) {
+	if (nums.empty()) {
+		throw runtime_error("wrong input");
+	}
+
+	int length = nums.size();
+	int maxSum = nums[0];
+	int currentSum = nums[0];
+
+	for (int i = 1; i < length; ++i)
+	{
+		if (currentSum <= 0) {
+			currentSum = nums[i];
+		}
+		else {
+			currentSum += nums[i];
+		}
+
+		maxSum = (currentSum > maxSum) ? currentSum : maxSum;
+	}
+
+	return maxSum;
+}
