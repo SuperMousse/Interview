b83c983c1e4bbe46c35b0995c38939f9335c5415
SuperMousse <lcq286227341@qq.com>
2019-11-30 17:04:23 +0800

    Update MaximumSubarray.cpp

--- LeetCode/53_MaximumSubarray/MaximumSubarray.cpp
+++ LeetCode/53_MaximumSubarray/MaximumSubarray.cpp
@@ -26,3 +26,34 @@ int maxSubArray(vector<int>& nums) {
 
 // Divide and Conquer, 分治法
 // 时间复杂度O(NlogN)
+
+// 最大子数组一定为中点左侧最大子数组, 中点右侧最大子数组, 跨越中点的所有子数组之中的一个
+int maxSubArray(vector<int>& nums) {
+	return maxSubArray(nums, 0, nums.size() - 1);
+}
+
+int maxSubArray(vector<int> & nums, int left, int right) {
+	if (left > right) {
+		return INT_MIN;
+	}
+	int middle = left + (right - left) / 2;
+	int middleLeft = 0;
+	int middleRight = 0;
+	int leftMax = maxSubArray(nums, left, middle - 1);
+	int rightMax = maxSubArray(nums, middle + 1, right);
+
+	// the maximum subarray spans the two halves
+	// starting from the middle element and move to both ends
+	// the max subarray on the left
+	for (int i = middle - 1, sum = 0; i >= left; --i) {
+		sum += nums[i];
+		middleLeft = max(sum, middleLeft);
+	}
+	for (int i = middle + 1, sum = 0; i <= right; ++i) {
+		sum += nums[i];
+		middleRight = max(sum, middleRight);
+	}
+	// left sub array VS right sub array VS cross sub array
+	int crossMax = middleLeft + middleRight + nums[middle];
+	return max(max(leftMax, rightMax), crossMax);
+}
