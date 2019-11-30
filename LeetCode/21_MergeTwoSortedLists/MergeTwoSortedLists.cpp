6a57f292fc70f75fb7e97829e4a37f6448501fa5
SuperMousse <lcq286227341@qq.com>
2019-11-30 15:56:30 +0800

    Update MergeTwoSortedLists.cpp

--- LeetCode/21_MergeTwoSortedLists/MergeTwoSortedLists.cpp
+++ LeetCode/21_MergeTwoSortedLists/MergeTwoSortedLists.cpp
@@ -21,4 +21,47 @@ ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
 	}
   
   
-  // 非递归
+// 非递归
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+		if (l1 == nullptr) {
+			return l2;
+		}
+		else if (l2 == nullptr) {
+			return l1;
+		}
+
+		ListNode* pNode1 = l1;
+		ListNode* pNode2 = l2;
+		ListNode* pMergedHead = nullptr;
+		if (pNode1->val < pNode2->val) {
+			pMergedHead = pNode1;
+			pNode1 = pNode1->next;
+		}
+		else {
+			pMergedHead = pNode2;
+			pNode2 = pNode2->next;
+		}
+		ListNode* pCurrent = pMergedHead;
+
+		while (pNode1 != nullptr && pNode2 != nullptr) {
+			if (pNode1->val < pNode2->val) {
+				pCurrent->next = pNode1;
+				pCurrent = pCurrent->next;
+				pNode1 = pNode1->next;
+			}
+			else {
+				pCurrent->next = pNode2;
+				pCurrent = pCurrent->next;
+				pNode2 = pNode2->next;
+			}
+		}
+
+		if (pNode1 == nullptr && pNode2 != nullptr) {
+			pCurrent->next = pNode2;
+		}
+		if (pNode1 != nullptr && pNode2 == nullptr) {
+			pCurrent->next = pNode1;
+		}
+
+		return pMergedHead;
+	}
