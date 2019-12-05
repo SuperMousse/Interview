ce8ea4297e7055b6f6732ca77c32cc96f8dbf51e
SuperMousse <lcq286227341@qq.com>
2019-12-05 22:43:47 +0800

    Create DepthOfBinaryTree.cpp

---
+++ LeetCode/104_DepthOfBinaryTree/DepthOfBinaryTree.cpp
@@ -0,0 +1,39 @@
+// 深度优先
+int TreeDepth(TreeNode* pRoot)
+{
+	if (pRoot == nullptr) {
+		return 0;
+	}
+	int nLeft = TreeDepth(pRoot->left);
+	int nRight = TreeDepth(pRoot->right);
+
+	return (nLeft > nRight) ? (nLeft + 1) : (nRight + 1);
+}
+
+
+// 广度优先
+ int maxDepth(TreeNode* root)
+	 {
+		 if (root == nullptr)
+			 return 0;
+
+		 int res = 0;
+		 queue<TreeNode*> queueTreeNode;
+		 queueTreeNode.push(root);
+		 while (!queueTreeNode.empty())
+		 {
+			 ++res;
+			 for (int i = 0, n = queueTreeNode.size(); i < n; ++i)
+			 {
+				 TreeNode* pNode = queueTreeNode.front();
+				 queueTreeNode.pop();
+
+				 if (pNode->left != nullptr)
+					 queueTreeNode.push(pNode->left);
+				 if (pNode->right != nullptr)
+					 queueTreeNode.push(pNode->right);
+			 }
+		 }
+
+		 return res;
+	 }
