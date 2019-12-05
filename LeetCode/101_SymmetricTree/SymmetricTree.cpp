a51b2da656a884ed30ce3076bd4029a94ff23b84
SuperMousse <lcq286227341@qq.com>
2019-12-05 22:18:31 +0800

    Update SymmetricTree.cpp

--- LeetCode/101_SymmetricTree/SymmetricTree.cpp
+++ LeetCode/101_SymmetricTree/SymmetricTree.cpp
@@ -1,1 +1,20 @@
-xxx
+// 递归 
+bool isSymmetric(TreeNode* root) {
+		 return isSymmetricCore(root, root);
+	 }
+
+	 bool isSymmetricCore(TreeNode* left, TreeNode* right) {
+		 if (left == nullptr && right == nullptr) {
+			 return true;
+		 }
+		 if (left == nullptr || right == nullptr) {
+			 return false;
+		 }
+		 if (left->val != right->val) {
+			 return false;
+		 }
+		 return isSymmetricCore(left->left, right->right) && isSymmetricCore(left->right, right->left);
+	 }
+
+
+// 非递归
