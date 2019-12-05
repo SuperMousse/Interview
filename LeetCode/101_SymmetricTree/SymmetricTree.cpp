d291ba99429da798947f2e6911d093683672e302
SuperMousse <lcq286227341@qq.com>
2019-12-05 22:22:46 +0800

    Update SymmetricTree.cpp

--- LeetCode/101_SymmetricTree/SymmetricTree.cpp
+++ LeetCode/101_SymmetricTree/SymmetricTree.cpp
@@ -18,3 +18,36 @@ bool isSymmetric(TreeNode* root) {
 
 
 // 非递归
+bool isSymmetric(TreeNode* root) {
+		 if (root == nullptr) {
+			 return true;
+		 }
+		 stack<TreeNode*> stackTreeNode;
+		 TreeNode* pNodeLeft = root;
+		 TreeNode* pNodeRight = root;
+		 stackTreeNode.push(pNodeLeft);
+		 stackTreeNode.push(pNodeRight);
+		 while (!stackTreeNode.empty()) {
+			 pNodeLeft = stackTreeNode.top();
+			 stackTreeNode.pop();
+			 pNodeRight = stackTreeNode.top();
+			 stackTreeNode.pop();
+			 if (pNodeLeft == nullptr && pNodeRight == nullptr) {
+				 continue;
+			 }
+			 if (pNodeLeft == nullptr || pNodeRight == nullptr) {
+				 return false;
+			 }
+
+			 if (pNodeLeft->val != pNodeRight->val) {
+				 return false;
+			 }
+
+			 stackTreeNode.push(pNodeLeft->left);
+			 stackTreeNode.push(pNodeRight->right);
+			 stackTreeNode.push(pNodeLeft->right);
+			 stackTreeNode.push(pNodeRight->left);
+		 }
+
+		 return true;
+	 }
