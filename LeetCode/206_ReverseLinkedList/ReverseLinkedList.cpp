cd16dc60769f9a86e155cfd61eb89ba945f27e53
SuperMousse <lcq286227341@qq.com>
2019-12-06 21:29:05 +0800

    Update ReverseLinkedList.cpp

--- LeetCode/206_ReverseLinkedList/ReverseLinkedList.cpp
+++ LeetCode/206_ReverseLinkedList/ReverseLinkedList.cpp
@@ -1,1 +1,28 @@
-xxx
+// 非递归
+
+class Solution {
+ public:
+	 ListNode* reverseList(ListNode* head) {
+		 if (head == nullptr) {
+			 return nullptr;
+		 }
+		 ListNode* pHead = nullptr;
+		 ListNode* pCurrent = head;
+		 ListNode* pFront = nullptr;
+		 ListNode* pBehind = nullptr;
+		 while (pCurrent != nullptr) {
+			 pFront = pCurrent->next;
+			 if (pFront == nullptr) {
+				 pHead = pCurrent;
+			 }
+			 pCurrent->next = pBehind;
+
+			 pBehind = pCurrent;
+			 pCurrent = pFront;
+		 }
+	 
+		 return pHead;
+	 }
+ };
+
+// 递归
