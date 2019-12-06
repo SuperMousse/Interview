fc5fb5e30e518d0a872e034680cd5a85b63936bb
SuperMousse <lcq286227341@qq.com>
2019-12-06 09:42:16 +0800

    Create LinkedListCycle.cpp

---
+++ LeetCode/141_LinkedListCycle/LinkedListCycle.cpp
@@ -0,0 +1,60 @@
+// 剑指offer解法
+ bool hasCycle(ListNode* head) {
+		 if (head == nullptr) {
+			 return false;
+		 }
+		 ListNode* slow = head->next;
+		 if (slow == nullptr) {
+			 return false;
+		 }
+		 ListNode* fast = slow->next;
+		 while (slow != nullptr && fast != nullptr) {
+			 if (slow == fast) {
+				 return true;
+			 }
+			 slow = slow->next;
+			 fast = fast->next;
+			 if (fast != nullptr) {
+				 fast = fast->next;
+			 }
+		 }
+		 return false;
+	 }
+   
+   
+// 我的解法
+bool hasCycle(ListNode* head) {
+		 if (head == nullptr) {
+			 return false;
+		 }
+		 ListNode* slow = head;
+		 ListNode* fast = head;
+		 if (fast->next != nullptr) {
+			 fast = fast->next;
+			 if (fast->next != nullptr) {
+				 fast = fast->next;
+			 }
+			 else {
+				 return false;
+			 }
+		 }
+		 else {
+			 return false;
+		 }
+		 while (slow != fast) {
+			 slow = slow->next;
+			 if (fast->next != nullptr) {
+				 fast = fast->next;
+				 if (fast->next != nullptr) {
+					 fast = fast->next;
+				 }
+				 else {
+					 return false;
+				 }
+			 }
+			 else {
+				 return false;
+			 }
+		 }
+		 return true;
+	 }
