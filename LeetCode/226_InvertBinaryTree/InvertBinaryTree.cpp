245f41d7ae995a0ac6e9e9747b7be84d7014c804
SuperMousse <lcq286227341@qq.com>
2019-12-15 10:28:09 +0800

    Update InvertBinaryTree.cpp

--- LeetCode/226_InvertBinaryTree/InvertBinaryTree.cpp
+++ LeetCode/226_InvertBinaryTree/InvertBinaryTree.cpp
@@ -1,4 +1,4 @@
-// 递归自底向上
+// 递归自底向上, 后序
 	 TreeNode* invertTree(TreeNode* root) {
 		 if (root == nullptr) {
 			 return nullptr;
@@ -17,4 +17,25 @@
 		 return root;
 	 }
    
-  // 非递归
+  // 非递归, 先序
+	 TreeNode* invertTree(TreeNode* root) {
+		 if (root == nullptr) {
+			 return nullptr;
+		 }
+
+		 stack<TreeNode*> stackTreeNode;
+		 stackTreeNode.push(root);
+		 while (!stackTreeNode.empty()) {
+			 TreeNode* pNode = stackTreeNode.top();
+			 stackTreeNode.pop();
+			 if (pNode != nullptr) {
+				 stackTreeNode.push(pNode->left);
+				 stackTreeNode.push(pNode->right);
+				 TreeNode* temp = pNode->left;
+				 pNode->left = pNode->right;
+				 pNode->right = temp;
+			 }
+		 }
+
+		 return root;
+	 }
