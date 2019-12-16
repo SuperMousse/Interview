5c889addd00f4c9164d5c37c9a61ac6a808a9b7f
SuperMousse <lcq286227341@qq.com>
2019-12-16 16:21:40 +0800

    Update MergeTwoBinaryTrees.cpp

--- LeetCode/617_MergeTwoBinaryTrees/MergeTwoBinaryTrees.cpp
+++ LeetCode/617_MergeTwoBinaryTrees/MergeTwoBinaryTrees.cpp
@@ -1,1 +1,10 @@
-xxx
+
+TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+ if (t1 == nullptr || t2 == nullptr) {
+   return t1 ? t1 : t2;
+ }
+ TreeNode* pNode = new TreeNode(t1->val + t2->val);
+ pNode->left = mergeTrees(t1->left, t2->left);
+ pNode->right = mergeTrees(t1->right, t2->right);
+ return pNode;
+}
