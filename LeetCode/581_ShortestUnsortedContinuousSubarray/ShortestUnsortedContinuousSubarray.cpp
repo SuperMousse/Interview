d0a7dbe699cf0a40ceb78f9457564f91de94896a
SuperMousse <lcq286227341@qq.com>
2019-12-16 10:46:34 +0800

    Update ShortestUnsortedContinuousSubarray.cpp

--- LeetCode/581_ShortestUnsortedContinuousSubarray/ShortestUnsortedContinuousSubarray.cpp
+++ LeetCode/581_ShortestUnsortedContinuousSubarray/ShortestUnsortedContinuousSubarray.cpp
@@ -1,5 +1,21 @@
 // 解法一: 排序, 时间复杂度O(NlogN)
+int findUnsortedSubarray(vector<int>& nums) {
+		 if (nums.empty()) {
+			 return 0;
+		 }
+		 vector<int> numsCopy(nums);
+		 sort(numsCopy.begin(), numsCopy.end());
+		 int left = 0;
+		 int right = nums.size() - 1;
+		 while (left < nums.size() && nums[left] == numsCopy[left]) {
+			 ++left;
+		 }
+		 while (right > left && nums[right] == numsCopy[right]) {
+			 --right;
+		 }
 
+		 return (right - left + 1);
+	 }
 
 // 解法二: 双端扫描, 时间复杂度O(N)
 
