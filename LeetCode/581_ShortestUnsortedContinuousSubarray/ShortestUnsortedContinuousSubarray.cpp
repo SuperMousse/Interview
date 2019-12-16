ad50e0840610207478e52871eeacc56e1605ebd1
SuperMousse <lcq286227341@qq.com>
2019-12-16 14:58:23 +0800

    Update ShortestUnsortedContinuousSubarray.cpp

--- LeetCode/581_ShortestUnsortedContinuousSubarray/ShortestUnsortedContinuousSubarray.cpp
+++ LeetCode/581_ShortestUnsortedContinuousSubarray/ShortestUnsortedContinuousSubarray.cpp
@@ -18,4 +18,40 @@ int findUnsortedSubarray(vector<int>& nums) {
 	 }
 
 
-// 解法二: 单端扫描, 时间复杂度O(N)
+// 解法二: 双端扫描, 时间复杂度O(N)
+int findUnsortedSubarray(vector<int>& nums) {
+		 if (nums.empty()) {
+			 return 0;
+		 }
+		 int left = 0;
+		 int right = nums.size() - 1;
+
+		 // nums[0, i - 1] and nums[j + 1, n - 1] are both sorted.
+		 while (left < right && nums[left] <= nums[left + 1]) { 
+			 ++left;
+		 }
+		 if (left >= right) {
+			 return 0;
+		 }
+		 while (nums[right] >= nums[right - 1]) {
+			 --right;
+		 }
+
+		 int max = INT_MIN;
+		 int min = INT_MAX;
+
+		 for (int i = left; i <= right; ++i) {
+			 max = max > nums[i] ? max : nums[i];
+			 min = min < nums[i] ? min : nums[i];
+		 }
+
+		 // nums[i - 1] <= min && max <= nums[j + 1], where min and max are the minimum and maximum values of subarray nums[i, j].
+		 while (left >= 0 && min < nums[left]) {
+			 --left;
+		 }
+		 while (right < nums.size() && nums[right] < max) {
+			 ++right;
+		 }
+		 // [left + 1, right - 1]闭区间为需要排序区间
+		 return (right - left - 1);
+	 }
