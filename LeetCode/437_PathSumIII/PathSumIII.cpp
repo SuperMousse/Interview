0c928d17b5089b9386ffc8abb7d71f93e534a0a9
SuperMousse <lcq286227341@qq.com>
2019-12-17 21:20:02 +0800

    Update PathSumIII.cpp

--- LeetCode/437_PathSumIII/PathSumIII.cpp
+++ LeetCode/437_PathSumIII/PathSumIII.cpp
@@ -1,1 +1,18 @@
-xxx
+// 解法一:
+
+
+
+// 解法二: Dual DFS
+
+public int pathSum(TreeNode root, int sum) {
+    if (root == null)
+      return 0;
+    return dfs(root, sum, 0) + pathSum(root.left, sum) + pathSum(root.right, sum);
+  }
+
+  private int dfs(TreeNode root, int sum, int cur) {
+    if (root == null)
+      return 0;
+    cur += root.val;
+    return (cur == sum ? 1 : 0) + dfs(root.left, sum, cur) + dfs(root.right, sum, cur);
+  }
