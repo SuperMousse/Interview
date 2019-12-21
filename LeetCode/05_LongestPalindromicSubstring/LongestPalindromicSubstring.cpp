04f5aac495929524900c10771f60bd734b80d1ec
SuperMousse <lcq286227341@qq.com>
2019-12-21 17:08:57 +0800

    Update LongestPalindromicSubstring.cpp

--- LeetCode/05_LongestPalindromicSubstring/LongestPalindromicSubstring.cpp
+++ LeetCode/05_LongestPalindromicSubstring/LongestPalindromicSubstring.cpp
@@ -28,5 +28,39 @@ string longestPalindrome(string s) {
 }
 
 
-// 解法二: 动态规划
-// 时间复杂度O(N)
+// 解法二：动态规划
+// 时间复杂度O(N^2), 空间复杂度O(1)
+// dp[i][j] = s[i]==s[j], j <= i + 1
+//            s[i]==s[j] && dp[i+1,j-1], j>i+1
+ string longestPalindrome(string s) {
+	 if (s.size() <= 1) {
+		 return s;
+	 }
+	 int length = s.size();
+	 bool* dp = new bool[length * length];
+	 memset(dp, 0, sizeof(bool) * length);
+ 
+	int start = 0;
+	int end = 0;
+
+	// i <= j
+	// because the calculation of dp[i + 1][j - 1], count i fron top to down to reuse.
+	for (int i = length - 1; i >= 0; --i)
+	{
+		for (int j = i; j < length; ++j)
+		{
+			// s[i]!=s[j], dp[i][j]=false
+			// s[i]=s[j], j-i<=1, dp[i][j]=true
+			// s[i]=s[j], j-i>1, dp[i][j]=dp[i + 1][j - 1]
+			dp[i * length + j] = (s[i] == s[j]) && (j - i < 2 || dp[(i + 1) * length + j - 1]);
+			// update panlindrome substring
+			if (dp[i * length + j] == true && (j - i) > (end - start)) {
+				start = i;
+				end = j;
+			}
+		}
+	}
+	delete[] dp;
+	return s.substr(start, (end - start + 1));
+}
+
