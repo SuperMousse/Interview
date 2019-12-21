7ab8b13b42d68fd4cb6c12ca7d495620b71b8aa1
SuperMousse <lcq286227341@qq.com>
2019-12-21 22:31:50 +0800

    Update CountingBits.cpp

--- LeetCode/338_CountingBits/CountingBits.cpp
+++ LeetCode/338_CountingBits/CountingBits.cpp
@@ -1,1 +1,10 @@
-xxx
+// 时间复杂度O(N), 空间复杂度O(N)
+// 利用x和x/2除了最后一位都相同的特点
+ vector<int> countBits(int num) {
+   vector<int> result(num + 1, 0);
+   for (int i = 1; i <= num; ++i)
+   {
+     result[i] = result[i >> 1] + (i & 0x1);
+   }
+   return result;
+ }
