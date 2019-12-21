3a02d71fe87b384dbc36c7303d7008d348e7d301
SuperMousse <lcq286227341@qq.com>
2019-12-21 11:08:14 +0800

    Update ContainerWithMostWater.cpp

--- LeetCode/11_ContainerWithMostWater/ContainerWithMostWater.cpp
+++ LeetCode/11_ContainerWithMostWater/ContainerWithMostWater.cpp
@@ -1,1 +1,16 @@
-xxx
+// 	 
+int maxArea(vector<int>& height) {
+		 int maxArea = 0;
+		 int left = 0;
+		 int right = height.size() - 1;
+		 while (left < right) {
+			 maxArea = max(maxArea, min(height[left], height[right]) * (right - left));
+			 if (height[left] < height[right]) {
+				 ++left;
+			 }
+			 else {
+				 --right;
+			 }
+		 }
+		 return maxArea;
+	 }
