6f5bcaf7e601b6b2be98a643f6951a96aba1483a
SuperMousse <lcq286227341@qq.com>
2019-12-21 21:50:46 +0800

    Update BinaryTreeInorderTraversal.cpp

--- LeetCode/94_BinaryTreeInorderTraversal/BinaryTreeInorderTraversal.cpp
+++ LeetCode/94_BinaryTreeInorderTraversal/BinaryTreeInorderTraversal.cpp
@@ -1,1 +1,26 @@
-xxx
+// 递归 
+vector<int> inorderTraversal(TreeNode* root) {
+   vector<int> result;
+   if (root == nullptr) {
+     return result;
+   }
+   helper(root, result);
+   return result;
+ }
+
+ void helper(TreeNode* root, vector<int>& result) {
+   if (root == nullptr) {
+     return;
+   }
+   if (root->left != nullptr) {
+     helper(root->left, result);
+   }
+   result.push_back(root->val);
+   if (root->right != nullptr) {
+     helper(root->right, result);
+   }
+   return;
+ }
+
+
+// 非递归
