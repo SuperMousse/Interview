4611eef93fc66ae3df93ca0e9bf28b125b1b69a1
SuperMousse <lcq286227341@qq.com>
2019-12-26 16:23:13 +0800

    Update JumpGame.cpp

--- LeetCode/55_JumpGame/JumpGame.cpp
+++ LeetCode/55_JumpGame/JumpGame.cpp
@@ -1,1 +1,19 @@
-xxx
+// 贪心算法
+// 时间复杂度O(N)
+// 从前向后考虑每个元素, 当前所能到达的最远距离是max(当前位置+当前元素, 最大距离)
+
+bool canJump(vector<int>& nums) {
+    if(nums.empty()){
+        return false;
+    }
+    int maxIndex = nums.size();
+    int farest = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+        if(farest < i) {
+            return false;
+        }
+        farest = max(i + nums[i], farest);
+    }
+
+    return true;
+}
