02b64d7da7428e1ec48f86f311d69d0a670d47bd
SuperMousse <lcq286227341@qq.com>
2019-12-26 16:04:34 +0800

    Update GroupAnagrams.cpp

--- LeetCode/49_GroupAnagrams/GroupAnagrams.cpp
+++ LeetCode/49_GroupAnagrams/GroupAnagrams.cpp
@@ -20,4 +20,45 @@ vector<vector<string>> groupAnagrams(vector<string>& strs) {
 
 
 // 时间复杂度O(N*M)
-//
+// 时间复杂度O(M*N)
+// 使用前27个质数, 将字母转换成对应质数作为hash code
+// 但是这种hash code编码效率比较低, 可以更换更高效的编码方法
+class Solution {
+public:
+        vector<vector<string>> groupAnagrams(vector<string>& strs) {
+            vector<vector<string> > result;
+            if(strs.empty()){
+                return result;
+            }
+            unordered_map<unsigned long, vector<string> > map;
+            for (string s : strs) {
+                unsigned long key = calHash(s);
+                map[key].push_back(s);
+            }
+            for (auto keyValue : map) {
+                result.push_back(keyValue.second);
+            }
+
+            for (auto i: result) {
+                for (auto j : i) {
+                    cout << j << " ";
+                }
+                cout << endl;
+            }
+
+            return result;
+        }
+
+        unsigned long calHash(string s) {
+            unsigned long hashCode = 1;
+            for (char c : s) {
+                hashCode *= hashTable[c-'a'];
+            }
+            return hashCode;
+        }
+    
+private:
+        unsigned long hashTable[27] = {2, 3, 5, 7, 11 ,13, 17, 19, 23, 29, 31, 37, 41, 43,
+                                      47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 107};
+
+};
