d0d25fe5be3047de699812f83ab0d734431911de
SuperMousse <lcq286227341@qq.com>
2019-12-30 18:03:48 +0800

    Update SortColors.cpp

--- CodingInterview/75_SortColors/SortColors.cpp
+++ CodingInterview/75_SortColors/SortColors.cpp
@@ -2,6 +2,26 @@
 
 
 // 解法二: 维护首尾双指针，移动双指针进行交换
+  void sortColors(vector<int>& nums) {
+      if(nums.empty()){
+          return;
+      }
+      int length = nums.size();
+      int left = 0;
+      int right = length - 1;
+      for (int i = 0; i <= right; ++i) {
+          if(nums[i] == 0) {
+              swap(nums[i], nums[left++]);
+          }
+          else if(nums[i] == 2) {
+              swap(nums[i], nums[right--]);
+          }
+          else {
+              continue;
+          }
 
+      }
+      return;
+  }
 
 //
