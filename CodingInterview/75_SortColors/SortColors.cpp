4b9bcad7ced9c054f95db5e235e94e09030abd39
SuperMousse <lcq286227341@qq.com>
2019-12-30 18:10:32 +0800

    Update SortColors.cpp

--- CodingInterview/75_SortColors/SortColors.cpp
+++ CodingInterview/75_SortColors/SortColors.cpp
@@ -2,26 +2,25 @@
 
 
 // 解法二: 维护首尾双指针，移动双指针进行交换
-  void sortColors(vector<int>& nums) {
-      if(nums.empty()){
-          return;
-      }
-      int length = nums.size();
-      int left = 0;
-      int right = length - 1;
-      for (int i = 0; i <= right; ++i) {
-          if(nums[i] == 0) {
-              swap(nums[i--], nums[left++]); // 注意需要i--，因为交换过去了新的元素之后需要重新考虑当前元素
-          }
-          else if(nums[i] == 2) {
-              swap(nums[i--], nums[right--]);
-          }
-          else {
-              continue;
-          }
+void sortColors(vector<int>& nums) {
+    if(nums.empty()){
+        return;
+    }
+    int length = nums.size();
+    int left = 0;
+    int right = length - 1;
+    for (int i = 0; i <= right; ++i) {
+        if(nums[i] == 0) {
+            swap(nums[i], nums[left++]); // 换到左侧的一定是0，当前元素一定比0大
+        }
+        else if(nums[i] == 2) {
+            swap(nums[i--], nums[right--]); // 从右侧换过来的元素可能是1，可能是2，需要重复考虑一下是否为2，要不要换到右侧
+        }
+        else {
+            continue;
+        }
 
-      }
-      return;
-  }
+    }
+    return;
+}
 
-//
