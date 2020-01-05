6128b44c76519eb7a1fb5974f095e5abb854ccad
SuperMousse <lcq286227341@qq.com>
2020-01-05 17:31:05 +0800

    Create BinaryTreeLevelOrderTraversal.cpp

---
+++ LeetCode/102_BinaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal.cpp
@@ -0,0 +1,28 @@
+	 vector<vector<int> > levelOrder(TreeNode* root) {
+		 if (root == nullptr) {
+			 return vector<vector<int> >();
+		 }
+		 vector<vector<int> > result;
+		 TreeNode* pNode = root;
+		 queue<TreeNode*> queueTreeNode;
+		 queueTreeNode.push(root);
+		 while (!queueTreeNode.empty()) {
+			 int length = queueTreeNode.size();
+			 vector<int> temp;
+			 for (int i = 0; i < length; ++i){
+				 pNode = queueTreeNode.front();
+				 queueTreeNode.pop();
+				 if (pNode != nullptr) {
+					 temp.push_back(pNode->val);
+				 }
+				 if (pNode->left != nullptr) {
+					 queueTreeNode.push(pNode->left);
+				 }
+				 if (pNode->right != nullptr) {
+					 queueTreeNode.push(pNode->right);
+				 }
+			 }
+			 result.push_back(temp);
+		 }
+		 return result;
+	 }
