760da62479fa4b48a523bb296b8b53390315ab3c
SuperMousse <lcq286227341@qq.com>
2020-01-16 15:29:06 +0800

    Create KthLargestElementInAnArray.cpp

---
+++ LeetCode/215_KthLargestElementInAnArray/KthLargestElementInAnArray.cpp
@@ -0,0 +1,24 @@
+// 方法一: multiset红黑树, 时间复杂度O(NlogK), 红黑树logK查找删除插入, N个节点
+int findKthLargest(vector<int>& nums, int k) {
+    if (nums.empty()) {
+        return 0;
+    }
+    multiset<int, greater<int> > KSet;
+    for(int num : nums) {
+        if (KSet.size() < k) {
+            KSet.insert(num);
+        }
+        else {
+            multiset<int, greater<int> >::iterator it = --KSet.end(); // 从小到大
+            if (num > *it) {
+                KSet.erase(it);
+                KSet.insert(num);
+            }
+        }
+    }
+    int result = *(--KSet.end());
+    return result;
+
+}
+
+// 方法二: 二分+partition, 时间复杂度O(2n)
