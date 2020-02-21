b623d8b5abaa25ba6f5dba956a72fea8010ba800
SuperMousse <lcq286227341@qq.com>
2020-02-21 11:45:52 +0800

    Update DailyTemperatures.cpp

--- LeetCode/739_DailyTemperatures/DailyTemperatures.cpp
+++ LeetCode/739_DailyTemperatures/DailyTemperatures.cpp
@@ -30,3 +30,22 @@ vector<int> dailyTemperatures(vector<int>& T) {
 }
 
 // 解法二: 栈
+vector<int> dailyTemperatures(vector<int>& T) {
+    if (T.empty()) {
+        return vector<int>();
+    }
+    int length = T.size();
+    vector<int> result(length, 0);
+    stack<int> stackInt; // stack的栈顶存储了下一个warmer的index
+    for (int i = length - 1; i >= 0; --i) {
+        // 说明当前天的温度比前一天的温度高, 那么就把一天的温度出栈, 向前一天, 直到
+        // 找到warmer的一天, 如果stack为空说明没有warmer的天了
+        while (!stackInt.empty() && T[i] >= T[stackInt.top()]) {
+            stackInt.pop();
+        }
+        result[i] = stackInt.empty() ? 0 : stackInt.top() - i;
+        stackInt.push(i);
+    }
+
+    return result;
+}
