55117728672d169904876c03084042261f2e43f3
SuperMousse <lcq286227341@qq.com>
2020-02-22 10:57:27 +0800

    Update dp.cpp

--- EveryDay/2020.2.22/dp.cpp
+++ EveryDay/2020.2.22/dp.cpp
@@ -30,3 +30,26 @@ int longestPalindromicSubsequence(string s) {
 
 
 // 最长增长子序列
+// dp[i]表示i位置上的最长增长子序列
+int longestIncreasingSubsequence(vector<int> nums) {
+    if (nums.empty()) {
+        return 0;
+    }
+    int length = nums.size();
+    int maxLength = 1;
+    vector<int> dp(length, -1);
+    dp[0] = 1;
+    // dp[i] = max(dp[j]) + 1, j \in [0, i)
+    // result = max(dp[j]), j \in [0, n)
+    for (int i = 0; i < length; ++i) {
+        int maxValue = 0;
+        for (int j = 0; j < i; ++j) {
+            if (nums[j] < nums[i]) {
+                maxValue = maxValue > dp[j] ? maxValue : dp[j];
+            }
+        }
+        dp[i] = maxValue + 1;
+        maxLength = maxLength > dp[i] ? maxLength : dp[i];
+    }
+    return maxLength;
+}
