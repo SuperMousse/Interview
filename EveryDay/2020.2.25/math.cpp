9d706dc40fd02f0d32f5c555a9f5bf389a1787c8
SuperMousse <lcq286227341@qq.com>
2020-02-24 22:00:25 +0800

    Update math.cpp

--- EveryDay/2020.2.25/math.cpp
+++ EveryDay/2020.2.25/math.cpp
@@ -5,7 +5,35 @@
 // 方法一: 求余数, 但无法适应大数
 // 方法二: 一直做减法
 
-
+int isMultipleOf3(int n) 
+{ 
+    int odd_count = 0; 
+    int even_count = 0; 
+  
+    /* Make no positive if +n is multiple of 3 then is -n. We are doing this to avoid 
+    stack overflow in recursion*/
+    if (n < 0) 
+        n = -n; 
+    if (n == 0) 
+        return 1; 
+    if (n == 1) 
+        return 0; 
+  
+    while (n) { 
+        /* If odd bit is set then 
+        increment odd counter */
+        if (n & 1) 
+            odd_count++; 
+  
+        /* If even bit is set then 
+        increment even counter */
+        if (n & 2) 
+            even_count++; 
+        n = n >> 2; 
+    } 
+  
+    return isMultipleOf3(abs(odd_count - even_count)); 
+} 
 
 
 // 对任意一个数字n高效乘7
