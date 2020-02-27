88902c6c11a2aba5d46d7fed2b9e7e4f9df2de86
SuperMousse <lcq286227341@qq.com>
2020-02-28 01:19:59 +0800

    Update dp.cpp

--- EveryDay/2020.2.26/dp.cpp
+++ EveryDay/2020.2.26/dp.cpp
@@ -28,6 +28,23 @@ int maxProduct(vector<int>& nums) {
 // Input: coins = [1, 2, 5], amount = 11
 // Output: 3 
 // Explanation: 11 = 5 + 5 + 1
-
+// dp[i] 表示价钱i的最少找钱数目
+// dp[i] = min(dp[i], dp[i - coins[j]] + 1) coins[j] <= i
+ int coinChange(vector<int>& coins, int amount) {
+    if (coins.empty() || amount < 0) {
+        return -1;
+    }
+    vector<int> dp(amount + 1, amount + 1);
+    dp[0] = 0;
+    for (int i = 1; i <= amount; ++i) {
+        int temp = 0;
+        for (int j = 0; j < coins.size(); ++j) {
+            if (coins[j] <= i) {
+                dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
+    }
+    return (dp[amount] < amount + 1) ? dp[amount] : -1;
+}
 
 
