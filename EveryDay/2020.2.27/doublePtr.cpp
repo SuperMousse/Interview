4ad8dfb32d7b75205899de9cc48c0eb0809878d3
SuperMousse <lcq286227341@qq.com>
2020-02-28 01:37:31 +0800

    Update doublePtr.cpp

--- EveryDay/2020.2.27/doublePtr.cpp
+++ EveryDay/2020.2.27/doublePtr.cpp
@@ -4,3 +4,22 @@
 
 
 // 无重复字符的最长子串
+int lengthOfLongestSubstring(string s) {
+    if (s.empty()) {
+        return 0;
+    }
+    vector<int> dp(256, -1);
+    int maxLength = 0;
+    int curLength = 0;
+    for (int i = 0; i < s.size(); ++i) {
+        if (dp[s[i]] == -1 || (i - dp[int(s[i])] > curLength)) {
+            ++curLength;
+        }
+        else {
+            curLength = i - dp[int(s[i])];
+        }
+        dp[int(s[i])] = i;
+        maxLength = maxLength > curLength ? maxLength : curLength;
+    }
+    return maxLength;
+}
