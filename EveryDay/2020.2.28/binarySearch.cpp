b4ad9f573530d979746e023f3d5b3c2035555fd8
SuperMousse <lcq286227341@qq.com>
2020-02-28 12:29:57 +0800

    Update binarySearch.cpp

--- EveryDay/2020.2.28/binarySearch.cpp
+++ EveryDay/2020.2.28/binarySearch.cpp
@@ -18,5 +18,30 @@ int findPeakElement(vector<int>& nums) {
 }
 
 // 解法二: 二分查找
+// 数组一直递增, 数组的峰值出现在末尾, 因为n+1被认为是-inf
+// 数组如果中间有peak, 那么就会产生在递增序列的最后一个元素上
+// 因此对于检测到递增时, 只需要向右检测
+// 否则向左检测
+int findPeakElement(vector<int>& nums) {
+    if (nums.empty()) {
+        return -1;
+    }
+    int length = nums.size();
+    int left = 0;
+    int right = length - 1;
+    while (left < right) {
+        int middle = (left + right) / 2;
+        int middleRight = middle + 1;
+        // middle < middleRight说明峰值在middleRight及其右侧
+        // 否则, 峰值在middle及其左侧
+        if (nums[middle] < nums[middleRight]) {
+            left = middleRight;
+        }
+        else {
+            right = middle;
+        }
+    }
+    return left;
+}
 
-//
+// 找到最接近的k个元素
