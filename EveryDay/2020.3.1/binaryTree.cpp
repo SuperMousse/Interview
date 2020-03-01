847db1a98a00a2ab9ff0a1ee2662485f33298a62
SuperMousse <lcq286227341@qq.com>
2020-03-01 23:16:48 +0800

    Update binaryTree.cpp

--- EveryDay/2020.3.1/binaryTree.cpp
+++ EveryDay/2020.3.1/binaryTree.cpp
@@ -1,1 +1,25 @@
-//
+// 验证一个二叉树是否是二叉搜索树
+
+bool isValidBST(TreeNode* root) {
+    if(root == nullptr) {
+        return true;
+    }
+    stack<TreeNode*> stackTreeNode;
+    TreeNode* pNode = root;
+    TreeNode* pPrev = nullptr;
+    // stackTreeNode.push(pNode); // push后需要移动, 不然下面push了两回
+    while (pNode != nullptr || !stackTreeNode.empty()) {
+        while (pNode != nullptr) {
+            stackTreeNode.push(pNode);
+            pNode = pNode->left;
+        }
+        pNode = stackTreeNode.top();
+        stackTreeNode.pop();
+        if (pPrev != nullptr && pNode->val <= pPrev->val) {
+            return false;
+        }
+        pPrev = pNode;
+        pNode = pNode->right;
+    }
+    return true;
+}
