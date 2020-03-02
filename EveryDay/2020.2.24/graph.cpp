50abd6d407548ce552b1d361ab9ea14864452c79
SuperMousse <lcq286227341@qq.com>
2020-03-02 18:28:30 +0800

    Update graph.cpp

--- EveryDay/2020.2.24/graph.cpp
+++ EveryDay/2020.2.24/graph.cpp
@@ -70,3 +70,52 @@ private:
 };
 
 // 课程安排
+// 检测是否存在入度为0的节点, 如果有入度为0的节点则可能完成,
+// 如果不存在入度为0的节点的话说明一定有环, 课程无法完成
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> Graph = buildGraph(numCourses, prerequisites);
+        vector<int> degrees = computeIndegrees(Graph);
+        for (int i = 0; i < numCourses; ++i) {
+            int j = 0;
+            for (; j < numCourses; ++j) {
+                // 存在入度为0的节点, 有可能完成
+                if (!degrees[j]) {
+                    break;
+                }
+            }
+            // 所有节点(不算已经置为-1的节点)的入度都不为0, 必有环, 无法完成
+            if (j == numCourses) {
+                return false;
+            }
+            // 对于找到的入度为0的节点, -1后其入度变为-1, 并将其出度节点入度-1
+            --degrees[j];
+            for (int v : Graph[j]) {
+                --degrees[v];
+            }
+
+        }
+        return true;
+    }
+private:
+    vector<vector<int>> buildGraph(int numCourses,
+                                   vector<vector<int>>& prerequisites) {
+        vector<vector<int>> Graph(numCourses,vector<int>());
+        for (auto edge : prerequisites) {
+            // 认为second是先导课程, second->first
+            Graph[edge[1]].push_back(edge[0]);
+        }
+        return Graph;
+    }
+    
+    vector<int> computeIndegrees(vector<vector<int>>& Graph) {
+        vector<int> degrees(Graph.size(), 0);
+        for (auto neighbours : Graph) {
+            for (int v : neighbours) {
+                degrees[v]++;
+            }
+        }
+        return degrees;
+    }
+};
