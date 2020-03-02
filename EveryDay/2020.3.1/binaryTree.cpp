cdd2e26a1fa96f1ceffed90fc16aa8ad23efde36
SuperMousse <lcq286227341@qq.com>
2020-03-02 11:15:34 +0800

    Update binaryTree.cpp

--- EveryDay/2020.3.1/binaryTree.cpp
+++ EveryDay/2020.3.1/binaryTree.cpp
@@ -23,3 +23,47 @@ bool isValidBST(TreeNode* root) {
     }
     return true;
 }
+
+// 从二叉树的先序好中序序列中重构二叉树
+TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    if (preorder.empty() || inorder.empty() ||
+        preorder.size() != inorder.size()) {
+        return nullptr;
+    }
+    int length = preorder.size();
+    TreeNode* head = helper(preorder, inorder, 0, length - 1, 0, length - 1);
+    return head;
+}
+TreeNode* helper(vector<int>& preorder, vector<int>& inorder,
+                 int beginPre, int endPre, int beginIn, int endIn) {
+    int rootVal = preorder[beginPre];
+    TreeNode* root = new TreeNode(rootVal);
+    root->left = root->right = nullptr;
+    if (beginPre == endPre) {
+        if (preorder[beginPre] = preorder[endPre]) {
+            return root;
+        }
+        else {
+            throw runtime_error("sth wrong");
+        }
+    }
+    int rootInIndex = beginIn;
+    while (rootInIndex <= endIn && inorder[rootInIndex] != rootVal) {
+        ++rootInIndex;
+    }
+    if (rootInIndex == endIn && inorder[rootInIndex] != inorder[endIn]) {
+        throw runtime_error("sth wrong");
+    }
+    int leftLength = rootInIndex - beginIn;
+    int leftEndPre = beginPre + leftLength;
+
+    if (leftLength > 0) {
+        root->left = helper(preorder, inorder,
+                            beginPre+1, leftEndPre, beginIn, rootInIndex-1);
+    }
+    if (beginPre + leftLength < endPre) {
+        root->right = helper(preorder, inorder,
+                             leftEndPre+1, endPre, rootInIndex+1, endIn);
+    }
+    return root;
+}
