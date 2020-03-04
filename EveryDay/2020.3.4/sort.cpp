8387236caea697d86709f36c79515eadac8c1150
SuperMousse <lcq286227341@qq.com>
2020-03-04 17:34:12 +0800

    Update sort.cpp

--- EveryDay/2020.3.4/sort.cpp
+++ EveryDay/2020.3.4/sort.cpp
@@ -6,3 +6,6 @@
 // required so that the maximum element is at the beginning and the 
 // minimum element is at last with the condition that only swapping 
 // of adjacent elements is allowed.
+// 找到左数最大的数字索引l, 右数最小的数字索引r
+// If l < r: Number of swaps = l + (n-r-1)
+// If l > r: Number of swaps = l + (n-r-2)
