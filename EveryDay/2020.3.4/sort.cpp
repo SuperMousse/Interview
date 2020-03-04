c1da34e4ffba164726b31a17aee08a7cdda6047a
SuperMousse <lcq286227341@qq.com>
2020-03-04 17:29:01 +0800

    Update sort.cpp

--- EveryDay/2020.3.4/sort.cpp
+++ EveryDay/2020.3.4/sort.cpp
@@ -2,3 +2,7 @@
 
 
 // Minimum adjacent swaps to move maximum and minimum to corners
+// Given N number of elements, find the minimum number of swaps 
+// required so that the maximum element is at the beginning and the 
+// minimum element is at last with the condition that only swapping 
+// of adjacent elements is allowed.
