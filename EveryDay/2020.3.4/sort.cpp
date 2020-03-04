c7c89696fef24f464354150564cef4f74f9d3213
SuperMousse <lcq286227341@qq.com>
2020-03-04 17:45:14 +0800

    Update sort.cpp

--- EveryDay/2020.3.4/sort.cpp
+++ EveryDay/2020.3.4/sort.cpp
@@ -9,3 +9,28 @@
 // 找到左数最大的数字索引l, 右数最小的数字索引r
 // If l < r: Number of swaps = l + (n-r-1)
 // If l > r: Number of swaps = l + (n-r-2)
+int minimumAdjacentSwaps(vector<int>& nums) {
+    if (nums.size() < 1) {
+        return 0;
+    }
+    int leftMaxIndex = 0;
+    int rightMinIndex = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+        // 大于号, 左数最大
+        if (nums[i] > nums[leftMaxIndex]) {
+            leftMaxIndex = i;
+        }
+        // 小于等于, 右数最小
+        if (nums[i] <= nums[rightMinIndex]) {
+            rightMinIndex = i;
+        }
+    }
+    if (leftMaxIndex < rightMinIndex) {
+        // 交换区间不重叠, max移动l次, min移动n-r-1次
+        return leftMaxIndex + (nums.size() - rightMinIndex - 1);
+    }
+    else {
+        // 交换区间重叠, min已经向右移动了一个了, 可以少移动一次
+        return leftMaxIndex + (nums.size() - rightMinIndex - 2);
+    }
+}
