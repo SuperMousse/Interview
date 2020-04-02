d2471e7857d810265dac9a10c0cb782b6e0e3512
SuperMousse <lcq286227341@qq.com>
2020-04-02 17:57:27 +0800

    Update BinaryTreeInorderTraversal.cpp

--- LeetCode/94_BinaryTreeInorderTraversal/BinaryTreeInorderTraversal.cpp
+++ LeetCode/94_BinaryTreeInorderTraversal/BinaryTreeInorderTraversal.cpp
@@ -99,3 +99,37 @@ vector<int> preorderTraversal(TreeNode* root) {
     reverse(result.begin(), result.end());
     return result;
 }
+
+
+// 后序遍历: 左 -> 右 -> 根
+// 需要在访问完左子节点后使用标记来标记右节点
+
+vector<int> postorderTraversal(TreeNode* root) {
+    if (root == nullptr) {
+        return vector<int>();
+    }
+    vector<int> result;
+    stack<TreeNode*> stackTree;
+    TreeNode* pNode = root;
+    TreeNode* pPrev = nullptr;
+    while (!stackTree.empty() || !pPrev) {
+        while (!pNode) {
+            stackTree.push(pNode);
+            pNode = pNode->left;
+        }
+        pNode = stackTree.top();
+        // 右节点访问过或者是空, 将根节点打印并且弹出
+        if (pNode->right == nullptr || pNode->right == pPrev) {
+            result.push_back(pNode->val);
+            stackTree.pop();
+            // 记录上一个访问过的节点, 用于判断访问根节点之前, 右子树是否已经访问过
+            pPrev = pNode;
+            // 表示不需要转向右子树, 继续弹出
+            pNode = nullptr;
+        }
+        else {
+            pNode = pNode->right;
+        }
+    }
+    return result;
+}
