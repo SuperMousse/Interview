61406c0d513ced45c3b2b7ea68879c130401df11
SuperMousse <lcq286227341@qq.com>
2020-04-04 19:51:11 +0800

    Update EditDistance.cpp

--- LeetCode/72_EditDistance/EditDistance.cpp
+++ LeetCode/72_EditDistance/EditDistance.cpp
@@ -1,1 +1,8 @@
-xx
+Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+
+You have the following 3 operations permitted on a word:
+
+1. Insert a character
+2. Delete a character
+3. Replace a character
+
