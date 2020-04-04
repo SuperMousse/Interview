44ef2dc79290d1a61f3677599034d10840d4aa4c
SuperMousse <lcq286227341@qq.com>
2020-04-04 21:31:49 +0800

    Update MinimumMovesToEqualArrayElements.cpp

--- LeetCode/453_MinimumMovesToEqualArrayElements/MinimumMovesToEqualArrayElements.cpp
+++ LeetCode/453_MinimumMovesToEqualArrayElements/MinimumMovesToEqualArrayElements.cpp
@@ -13,3 +13,8 @@ Only three moves are needed (remember each move increments two elements):
 
 [1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
 
+
+// 定义sum为数组中所有数字的和, m为变换的次数, n为数字的长度, x为最终变换的结果, minNum为数组中最小的数字
+// sum + m * (n - 1) = x * n
+// x = minNum + m, 因为数组中最小的数字经过变换后还是最小的, 他变换的次数就是所有数字的变换次数
+// 推出 m = sum - n * minNum
