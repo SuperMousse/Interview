b003dce16a3cb8b157f58d8f98b759e56ca7b342
SuperMousse <lcq286227341@qq.com>
2020-04-06 11:50:02 +0800

    Update TreeDepth.cpp

--- CodingInterview/55_01_TreeDepth/TreeDepth.cpp
+++ CodingInterview/55_01_TreeDepth/TreeDepth.cpp
@@ -49,3 +49,17 @@ int TreeDepth(TreeNode* pRoot)
 	 }
 
 // 树的最小深度
+    int minDepth(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        if (root->left == nullptr) {
+            return minDepth(root->right) + 1;
+        }
+        if (root->right == nullptr) {
+            return minDepth(root->left) + 1;
+        }
+        int left = minDepth(root->left);
+        int right = minDepth(root->right);
+        return (left < right ? left : right) + 1;
+    }
