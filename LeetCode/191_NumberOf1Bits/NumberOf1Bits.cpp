ad5a18ad68fcdcc6544189102cf0aa18d17707b3
SuperMousse <lcq286227341@qq.com>
2020-04-19 23:43:31 +0800

    Update NumberOf1Bits.cpp

--- LeetCode/191_NumberOf1Bits/NumberOf1Bits.cpp
+++ LeetCode/191_NumberOf1Bits/NumberOf1Bits.cpp
@@ -1,1 +1,11 @@
-xx
+int hammingWeight(uint32_t n) {
+	uint32_t mask = 1;
+	int count = 0;
+	for (int i = 0; i < 32; ++i) {
+		if ((n & mask) != 0) {
+			++count;
+		}
+		mask <<= 1;
+	}
+	return count;
+}
