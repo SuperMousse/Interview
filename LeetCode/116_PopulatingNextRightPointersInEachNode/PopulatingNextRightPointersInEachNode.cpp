3a8d900c4bca1e084fefd9f58afe1c4ae6c4accd
SuperMousse <lcq286227341@qq.com>
2020-06-20 23:58:20 +0800

    Update PopulatingNextRightPointersInEachNode.cpp

--- LeetCode/116_PopulatingNextRightPointersInEachNode/PopulatingNextRightPointersInEachNode.cpp
+++ LeetCode/116_PopulatingNextRightPointersInEachNode/PopulatingNextRightPointersInEachNode.cpp
@@ -1,3 +1,4 @@
+// 方法一： 二叉树层次遍历， 速度有点慢
 Node* connect(Node* root) {
     if (root == nullptr) {
         return nullptr;
@@ -22,6 +23,28 @@ Node* connect(Node* root) {
                 queueNode.push(pNode->right);
             }
         }
+    }
+    return root;
+}
+
+
+// 指针沿着深度和水平两个方向直接移动
+Node* connect(Node* root) {
+    if (root == nullptr) {
+        return nullptr;
+    }
+    Node* pPrev = root;
+    Node* pCurr = nullptr;
+    while(pPrev->left) {
+        pCurr = pPrev;
+        while (pCurr) {
+            pCurr->left->next = pCurr->right;
+            if (pCurr->next) {
+                pCurr->right->next = pCurr->next->left;
+            }
+            pCurr = pCurr->next;
+        }
+        pPrev = pPrev->left;
     }
     return root;
 }
