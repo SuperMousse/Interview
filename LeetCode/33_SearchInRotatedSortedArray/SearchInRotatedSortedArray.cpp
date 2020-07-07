1dbfc2bb591a35c71ff5319fda30fdf41ae1532b
SuperMousse <lcq286227341@qq.com>
2020-07-07 23:08:35 +0800

    Update SearchInRotatedSortedArray.cpp

--- LeetCode/33_SearchInRotatedSortedArray/SearchInRotatedSortedArray.cpp
+++ LeetCode/33_SearchInRotatedSortedArray/SearchInRotatedSortedArray.cpp
@@ -63,3 +63,39 @@
 	 }
 	 return middle;
  }
+
+
+
+
+// 解法二: 一次二分完成查找
+int search(vector<int>& nums, int target) {
+    if (nums.empty()) {
+        return -1;
+    }
+    int len = nums.size();
+    int left = 0;
+    int right = len - 1;
+    while (left <= right) {
+        int middle = left + (right - left) / 2;
+        if (nums[middle] == target) {
+            return middle;
+        }
+        if (nums[middle] >= nums[left]) {
+            if (target >= nums[left] && target < nums[middle]) {
+                right = middle - 1;
+            }
+            else {
+                left = middle + 1;
+            }
+        }
+        else {
+            if (target > nums[middle] && target <= nums[right]) {
+                left = middle + 1;
+            }
+            else {
+                right = middle - 1;
+            }
+        }
+    }
+    return -1;
+}
