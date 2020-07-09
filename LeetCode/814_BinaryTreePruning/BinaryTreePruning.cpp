814521d869997448caa6f96b5ad5cc53737e2a4d
SuperMousse <lcq286227341@qq.com>
2020-07-09 23:31:09 +0800

    Create BinaryTreePruning.cpp

---
+++ LeetCode/814_BinaryTreePruning/BinaryTreePruning.cpp
@@ -0,0 +1,11 @@
+TreeNode* pruneTree(TreeNode* root) {
+    if (root == nullptr) {
+        return nullptr;
+    }
+    root->left = pruneTree(root->left);
+    root->right = pruneTree(root->right);
+    if (root->left == nullptr && root->right == nullptr && root->val == 0) {
+        return nullptr;
+    }
+    return root;
+}
