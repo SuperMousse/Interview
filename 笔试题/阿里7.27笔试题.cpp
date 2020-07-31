719a70228304e547deab4c1d583fc0b0c55ff2da
SuperMousse <lcq286227341@qq.com>
2020-07-31 10:20:20 +0800

    Update 阿里7.27笔试题.cpp

--- 笔试题/阿里7.27笔试题.cpp
+++ 笔试题/阿里7.27笔试题.cpp
@@ -75,3 +75,20 @@ int main() {
 // 题二
 // https://www.nowcoder.com/discuss/460859?type=2
 // https://www.nowcoder.com/discuss/462000?source_id=profile_create&channel=1009
+
+// https://blog.csdn.net/qq_42232193/article/details/107634394
+
+// 有个藏宝架有n层，每层的宝物数量不一，每个宝物都有其价值，现在要求拿出m个宝物，并且需要遵守规则：
+// 每次只能拿选定层的两端的宝物, 要拿出的m个宝物的总价值是各种方案里最大的
+
+// 输入：
+// n m
+// 下面每行代表每层，且第一个数是这层宝物的数量k，后面的则是k个宝物的价值
+// 4 1 2 4 5
+// 5 1 2 4 5 5
+// 样例：
+// 2 3
+// 2 3 2
+// 4 1 4 1 5
+// 输出：5 + 3 + 2 = 10
+
