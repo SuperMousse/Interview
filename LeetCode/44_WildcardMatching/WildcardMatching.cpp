f1c070aa85ee6cd311404b360af9f7248d85733d
SuperMousse <lcq286227341@qq.com>
2020-08-05 00:35:12 +0800

    Update WildcardMatching.cpp

--- LeetCode/44_WildcardMatching/WildcardMatching.cpp
+++ LeetCode/44_WildcardMatching/WildcardMatching.cpp
@@ -8,21 +8,26 @@ bool isMatch(string s, string p) {
 
     for(int i = 0; i <= m; ++i){
         for(int j = 0; j <= n; ++j){
-            int startOfS = i - 1;
-            int startOfP = j - 1;
+            // 都是空元素， match
             if (i == 0 && j == 0) {
                 dp[i][j] = true;
             }
+            // s为空， dp[i][j]之前的dp[i][j-1]等都必须是true, 并且p中后续字符必须都是'*', 才为true
             else if (i == 0) {
-                dp[i][j] = p[startOfP] == '*' && dp[i][j-1];
+                dp[i][j] = (p[j-1] == '*' && dp[i][j-1]);
             }
+            // p为空, s不为空, false
             else if (j == 0) {
                 dp[i][j] = false;
             }
-            else if (p[startOfP] == '*') {
+            // s, p都不为空, p[j-1]为'*', 此时可以匹配多个字符, dp[i][j-1]表示s成功匹配一个字符, p不移动
+            // dp[i-1][j]表示'*'未匹配任何字符, s没移动, p移动了
+            else if (p[j-1] == '*') {
                 dp[i][j] = dp[i][j-1] || dp[i-1][j];
             }
-            else if (s[startOfS] == p[startOfP] || p[startOfP] == '?') {
+            // s, p都不为空, s[i-1]/p[j-1]字符相同成功匹配， 或者p[j-1]为'?'可以匹配任意字符,
+            // 此时dp[i][j]依赖于dp[i-1][j-1]
+            else if (s[i-1] == p[j-1] || p[j-1] == '?') {
                 dp[i][j] = dp[i-1][j-1];
             }
         }
