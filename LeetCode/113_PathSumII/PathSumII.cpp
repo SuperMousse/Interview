85d6ce901f199a06e2f8288b8da9c87660e775ab
SuperMousse <lcq286227341@qq.com>
2020-08-14 21:45:17 +0800

    Create PathSumII.cpp

---
+++ LeetCode/113_PathSumII/PathSumII.cpp
@@ -0,0 +1,44 @@
+// 和剑指offer path in tree 相同
+
+class Solution {
+public:
+    vector<vector<int> > pathSum(TreeNode* root, int expectNumber) {
+        vector<vector<int>> result;
+        if (root == nullptr) {
+            return result;
+        }
+        vector<int> path;
+        int currentSum = 0;
+        find(root, expectNumber, result, path, currentSum);
+        return result;
+    }
+    
+    void find(TreeNode* root, int expectNumber,
+        vector<vector<int> >& result, vector<int>& path, int currentSum) {
+        if (root == nullptr) {
+            return;
+        }
+        currentSum += root->val;
+        path.push_back(root->val);
+
+        // 如果是叶子节点，并且路径上节点值的和等于输入的值，加入result
+        bool isLeaf = (root->left == nullptr && root->right == nullptr);
+        if (currentSum == expectNumber && isLeaf) {
+            result.push_back(path);
+        }
+        else {
+            // 如果不是叶子节点，则遍历它的所有子节点
+            if (root->left != nullptr) {
+                find(root->left, expectNumber, result, path, currentSum);
+            }
+            if (root->right != nullptr) {
+                find(root->right, expectNumber, result, path, currentSum);
+            }
+        }
+
+        // 在返回父节点之前，在路径上删除当前节点
+        path.pop_back();
+
+        return;
+    }
+};
