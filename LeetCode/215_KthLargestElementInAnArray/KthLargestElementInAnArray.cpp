ca803878076ac185d6410a20cd96e287f80c139b
SuperMousse <lcq286227341@qq.com>
2020-08-14 22:45:59 +0800

    Update KthLargestElementInAnArray.cpp

--- LeetCode/215_KthLargestElementInAnArray/KthLargestElementInAnArray.cpp
+++ LeetCode/215_KthLargestElementInAnArray/KthLargestElementInAnArray.cpp
@@ -1,4 +1,31 @@
-// 方法一: multiset红黑树, 时间复杂度O(NlogK), 红黑树logK查找删除插入, N个节点
+// 方法一： 小顶堆
+
+int findKthLargest(vector<int>& nums, int k) {
+    if (nums.empty() || k < 1 || nums.size() < k) {
+        return -1;
+    }
+    vector<int> heap;
+    for (int i = 0; i < k; ++i) {
+        heap.push_back(nums[i]);
+    }
+    make_heap(heap.begin(), heap.end(), greater<int>());
+
+    for (int i = k; i < nums.size(); ++i) {
+        int top = heap[0];
+        if (nums[i] > top) {
+            pop_heap(heap.begin(), heap.end(), greater<int>());
+            heap.pop_back();
+            heap.push_back(nums[i]);
+            push_heap(heap.begin(), heap.end(), greater<int>());
+        }
+    }
+
+    return heap[0];
+}
+
+
+
+// 方法二: multiset红黑树, 时间复杂度O(NlogK), 红黑树logK查找删除插入, N个节点
 int findKthLargest(vector<int>& nums, int k) {
     if (nums.empty()) {
         return 0;
@@ -21,4 +48,4 @@ int findKthLargest(vector<int>& nums, int k) {
 
 }
 
-// 方法二: 二分+partition, 时间复杂度O(2n)
+// 方法三: 二分+partition, 时间复杂度O(2n)
