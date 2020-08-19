dec7a52aaf08a9f89226941af39de8e235252f75
SuperMousse <lcq286227341@qq.com>
2020-08-19 12:01:01 +0800

    Update LinkedListCycle.cpp

--- LeetCode/141_LinkedListCycle/LinkedListCycle.cpp
+++ LeetCode/141_LinkedListCycle/LinkedListCycle.cpp
@@ -23,42 +23,21 @@
    
    
 // 我的解法
-bool hasCycle(ListNode* head) {
-		 if (head == nullptr) {
-			 return false;
-		 }
-		 ListNode* slow = head;
-		 ListNode* fast = head;
-		 if (fast->next != nullptr) {
-			 fast = fast->next;
-			 if (fast->next != nullptr) {
-				 fast = fast->next;
-			 }
-			 else {
-				 return false;
-			 }
-		 }
-		 else {
-			 return false;
-		 }
-		 while (slow != fast) {
-			 slow = slow->next;
-			 if (fast->next != nullptr) {
-				 fast = fast->next;
-				 if (fast->next != nullptr) {
-					 fast = fast->next;
-				 }
-				 else {
-					 return false;
-				 }
-			 }
-			 else {
-				 return false;
-			 }
-		 }
-		 return true;
-	 }
-
+bool hasCycle(ListNode *head) {
+    if (head == nullptr || head->next == nullptr) {
+        return false;
+    }
+    ListNode* slow = head;
+    ListNode* fast = head->next;
+    while (fast->next != nullptr && fast->next->next != nullptr) {
+        if (slow == fast) {
+            return true;
+        }
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return false;
+}
 
 // 额外解法
 // 使用哈希表记录节点是否访问过, 若访问过则存在环
