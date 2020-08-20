774786e922d70cce635f633d44d37970b9eaf500
SuperMousse <lcq286227341@qq.com>
2020-08-20 17:14:33 +0800

    Update 1464_MaximumProductOfTwoElementsInAnArray.cpp

--- LeetCode/1464_MaximumProductOfTwoElementsInAnArray/1464_MaximumProductOfTwoElementsInAnArray.cpp
+++ LeetCode/1464_MaximumProductOfTwoElementsInAnArray/1464_MaximumProductOfTwoElementsInAnArray.cpp
@@ -1,3 +1,9 @@
+// 给定数组, 选择两个不同的索引 i, j, 返回(nums[i]-1)*(nums[j]-1)的最大值
+// Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+
+// Input: nums = [1,5,4,5]
+// Output: 16
+// Explanation: Choosing the indices i=1 and j=3 (indexed from 0), you will get the maximum value of (5-1)*(5-1) = 16.
 
 int maxProduct(vector<int>& nums) {
     if (nums.size() < 2) {
