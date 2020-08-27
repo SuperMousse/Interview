e3b892577dbba9b1a2c094774ff24a0995830dd2
SuperMousse <lcq286227341@qq.com>
2020-08-27 10:26:33 +0800

    Update LinkedListCycleII.cpp

--- LeetCode/142_LinkedListCycleII/LinkedListCycleII.cpp
+++ LeetCode/142_LinkedListCycleII/LinkedListCycleII.cpp
@@ -1,37 +1,39 @@
-    ListNode *detectCycle(ListNode *head) {
-        if(head == nullptr || head->next == nullptr) {
-            return nullptr;
-        }
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        // make sure there is a cycle
-        while(slow != fast && fast->next != nullptr &&
-              fast->next->next != nullptr) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        if(fast->next == nullptr || fast->next->next == nullptr){
-            return nullptr;
-        }
-        
-        // calcalate the length of cycle
-        int lenOfCycle = 1;
-        fast = fast->next;
-        while(slow != fast) {
-            fast = fast->next;
-            ++lenOfCycle;
-        }
-        
-        // find the entry of cycle
-        slow = head;
-        fast = head;
-        while(lenOfCycle > 0) {
-            fast = fast->next;
-            --lenOfCycle;
-        }
-        while(slow != fast) {
-            slow = slow->next;
-            fast = fast->next;
-        }
-        return slow;
+ListNode *detectCycle(ListNode *head) {
+    if(head == nullptr || head->next == nullptr) {
+        return nullptr;
     }
+    ListNode* slow = head;
+    ListNode* fast = head->next;
+    // make sure there is a cycle
+    while(slow != fast && fast->next != nullptr &&
+          fast->next->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    if(fast->next == nullptr || fast->next->next == nullptr){
+        return nullptr;
+    }
+
+    // calcalate the length of cycle
+    int lenOfCycle = 1;
+    fast = fast->next;
+    while(slow != fast) {
+        fast = fast->next;
+        ++lenOfCycle;
+    }
+
+    // find the entry of cycle
+    slow = head;
+    fast = head;
+    while(lenOfCycle > 0) {
+        fast = fast->next;
+        --lenOfCycle;
+    }
+    while(slow != fast) {
+        slow = slow->next;
+        fast = fast->next;
+    }
+    return slow;
+}
+
+// 也可以直接在相遇后, slow=head, fast不变, 两指针相遇即为入口
