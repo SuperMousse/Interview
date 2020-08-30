e578681bfcb79cc4a7b1278ef6a2b169a145b2b9
SuperMousse <lcq286227341@qq.com>
2020-08-30 11:25:25 +0800

    Create CourseSchedule.cpp

---
+++ LeetCode/207_CourseSchedule/CourseSchedule.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> Graph = buildGraph(numCourses, prerequisites);
+        vector<int> degrees = computeIndegrees(Graph);
+        for (int i = 0; i < numCourses; ++i) {
+            int j = 0;
+            for (; j < numCourses; ++j) {
+                // 存在入度为0的节点, 有可能完成
+                if (!degrees[j]) {
+                    break;
+                }
+            }
+            // 所有节点的入度都不为0, 必有环, 无法完成
+            if (j == numCourses) {
+                return false;
+            }
+            // 对于找到的入度为0的节点, -1后其入度变为-1, 并将其邻域节点入度-1
+            --degrees[j];
+            for (int v : Graph[j]) {
+                --degrees[v];
+            }
+
+        }
+        return true;
+    }
+private:
+    vector<vector<int>> buildGraph(int numCourses,
+                                   vector<vector<int>>& prerequisites) {
+        vector<vector<int>> Graph(numCourses,vector<int>());
+        for (auto edge : prerequisites) {
+            // 认为second是先导课程, second->first
+            Graph[edge[1]].push_back(edge[0]);
+        }
+        return Graph;
+    }
+    
+    vector<int> computeIndegrees(vector<vector<int>>& Graph) {
+        vector<int> degrees(Graph.size(), 0);
+        for (auto neighbours : Graph) {
+            for (int v : neighbours) {
+                degrees[v]++;
+            }
+        }
+        return degrees;
+    }
+};
