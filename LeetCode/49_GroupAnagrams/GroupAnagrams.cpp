09915779bc409bc18e59ed58a97c73e03d5626b9
SuperMousse <lcq286227341@qq.com>
2020-09-06 22:38:18 +0800

    Update GroupAnagrams.cpp

--- LeetCode/49_GroupAnagrams/GroupAnagrams.cpp
+++ LeetCode/49_GroupAnagrams/GroupAnagrams.cpp
@@ -1,24 +1,3 @@
-// 时间复杂度O(N*排序)
-// 使用排序的字符串作为key, 输出作为value
-
-vector<vector<string>> groupAnagrams(vector<string>& strs) {
-    vector<vector<string> > result;
-    if(strs.empty()){
-        return result;
-    }
-    unordered_map<string, vector<string> > map;
-    for (string s : strs) {
-        string temp = s;
-        sort(temp.begin(), temp.end());
-        map[temp].push_back(s);
-    }
-    for (pair<string, vector<string> > keyValue : map) {
-        result.push_back(keyValue.second);
-    }
-    return result;
-}
-
-
 // 时间复杂度O(N*M)
 // 时间复杂度O(M*N)
 // 使用前27个质数, 将字母转换成对应质数作为hash code
@@ -62,3 +41,27 @@ private:
                                       47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 107};
 
 };
+
+
+// 时间复杂度O(N*排序)
+// 使用排序的字符串作为key, 输出作为value
+
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    vector<vector<string> > result;
+    if(strs.empty()){
+        return result;
+    }
+    unordered_map<string, vector<string> > map;
+    for (string s : strs) {
+        string temp = s;
+        sort(temp.begin(), temp.end());
+        map[temp].push_back(s);
+    }
+    for (pair<string, vector<string> > keyValue : map) {
+        result.push_back(keyValue.second);
+    }
+    return result;
+}
+
+
+
