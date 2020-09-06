4f70450d1bccc425ae31a94a6cdfa0188eee8eea
SuperMousse <lcq286227341@qq.com>
2020-09-06 22:49:24 +0800

    Update DailyTemperatures.cpp

--- LeetCode/739_DailyTemperatures/DailyTemperatures.cpp
+++ LeetCode/739_DailyTemperatures/DailyTemperatures.cpp
@@ -5,7 +5,29 @@
 // nedd inedx:       [ 1,  2,  6,  5,  5,  6,  6,  7]
 // result:           [ 1,  1,  4,  2,  1,  1,  0,  0]
 
-// 解法一: 时间复杂度O(NW), 空间复杂度O(N+W), W是T允许的数目
+
+// 解法一: 栈
+vector<int> dailyTemperatures(vector<int>& T) {
+    if (T.empty()) {
+        return vector<int>();
+    }
+    int length = T.size();
+    vector<int> result(length, 0);
+    stack<int> stackInt; // stack的栈顶存储了下一个warmer的index
+    for (int i = length - 1; i >= 0; --i) {
+        // 说明当前天的温度比前一天的温度高, 那么就把一天的温度出栈, 向前一天, 直到
+        // 找到warmer的一天, 如果stack为空说明没有warmer的天了
+        while (!stackInt.empty() && T[i] >= T[stackInt.top()]) {
+            stackInt.pop();
+        }
+        result[i] = stackInt.empty() ? 0 : stackInt.top() - i;
+        stackInt.push(i);
+    }
+
+    return result;
+}
+
+// 解法二: 时间复杂度O(NW), 空间复杂度O(N+W), W是T允许的数目
 vector<int> dailyTemperatures(vector<int>& T) {
     if (T.empty()) {
         return vector<int>();
@@ -29,23 +51,3 @@ vector<int> dailyTemperatures(vector<int>& T) {
     return result;
 }
 
-// 解法二: 栈
-vector<int> dailyTemperatures(vector<int>& T) {
-    if (T.empty()) {
-        return vector<int>();
-    }
-    int length = T.size();
-    vector<int> result(length, 0);
-    stack<int> stackInt; // stack的栈顶存储了下一个warmer的index
-    for (int i = length - 1; i >= 0; --i) {
-        // 说明当前天的温度比前一天的温度高, 那么就把一天的温度出栈, 向前一天, 直到
-        // 找到warmer的一天, 如果stack为空说明没有warmer的天了
-        while (!stackInt.empty() && T[i] >= T[stackInt.top()]) {
-            stackInt.pop();
-        }
-        result[i] = stackInt.empty() ? 0 : stackInt.top() - i;
-        stackInt.push(i);
-    }
-
-    return result;
-}
