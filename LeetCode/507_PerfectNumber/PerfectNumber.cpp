2c4b9695c350b67a3b91bddfbdef6f6e711aebe2
SuperMousse <lcq286227341@qq.com>
2020-10-22 20:50:10 +0800

    Create PerfectNumber.cpp

---
+++ LeetCode/507_PerfectNumber/PerfectNumber.cpp
@@ -0,0 +1,12 @@
+bool checkPerfectNumber(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    int sum = 1;
+    for (int i = 2; i <= sqrt(num); ++i) {
+        if (num % i == 0) {
+            sum += (i + num / i);
+        }
+    }
+    return num == sum;
+}
