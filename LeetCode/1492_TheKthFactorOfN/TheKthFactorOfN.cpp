68c2d2b1e8e7f026478a766e0bec228b07d9b689
SuperMousse <lcq286227341@qq.com>
2020-11-19 11:17:25 +0800

    Update TheKthFactorOfN.cpp

--- LeetCode/1492_TheKthFactorOfN/TheKthFactorOfN.cpp
+++ LeetCode/1492_TheKthFactorOfN/TheKthFactorOfN.cpp
@@ -1,3 +1,4 @@
+// O(n)解法
 int kthFactor(int n, int k) {
     int count = 0;
     for (int i = 1; i <= n; ++i) {
@@ -9,4 +10,27 @@ int kthFactor(int n, int k) {
         }
     }
     return -1;
+}
+
+
+// O(sqrt(n))解法
+int kthFactor(int n, int k) {
+    int count = 0;
+    vector<int> data; // data中存储了大于sqrt(n)的因子, count计算了小于sqrt(n)的因子
+    for (int i = 1; i * i <= n; ++i) {
+        if (n % i == 0) {
+            if (i * i != n) {
+                data.push_back(n / i);
+            }
+            if (++count == k) {
+                return i;
+            }
+        }
+    }
+    if (data.size() + count < k) {
+        return -1;
+    }
+    // data.size() + count >= k, 说明第k个因子大于sqrt(n)
+    // 第k个因子是大于sqrt(n)的第(k - count)个, data倒序存储直接索引即可
+    return data[data.size() - (k - count)];
 }
