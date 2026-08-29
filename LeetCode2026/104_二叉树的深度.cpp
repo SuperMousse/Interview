7a2732cc0e688079ebf03d88a7e9346c8d890e50
SuperMousse <lcq286227341@qq.com>
2026-08-29 16:05:58 +0800

    Create 104_二叉树的深度.cpp

---
+++ LeetCode2026/104_二叉树的深度.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int depth = 0;
+        int left_depth = maxDepth(root->left);
+        int right_depth = maxDepth(root->right);
+        return (left_depth > right_depth) ? (left_depth+1) : (right_depth+1);
+
+    }
+};
